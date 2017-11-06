42340096d7524423abf449427854a850a4f4a20b
woboni <milli369@hotmail.com>
2017-11-06 07:27:22 +0100

    Add files via upload
    some corrections and added comments

--- src/Pi/webrobot.py
+++ src/Pi/webrobot.py
@@ -23,6 +23,9 @@ pygame.init()
 screen = pygame.display.set_mode((654,380))
 screen.fill((0, 0, 0))
 bat_prozent = 0
+bat_prozent_lock = threading.Lock()
+move_lock = threading.Lock() 
+
 
 #face informationen
 radius = 0.8
@@ -58,9 +61,6 @@ else:
 		UDP_IP = ''
 IP = ''
 
-#RFIDidentifier
-readRFIDnumber = "empty"
-
 #sound setup
 if pygame.mixer and not pygame.mixer.get_init():
     print ('Warning, no sound')
@@ -72,9 +72,40 @@ class dummysound:
 SendToApp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	
 """ FUNCTIONS """
+#loading sounds from subdir 'data'    
+def load_sound(file):
+	if not pygame.mixer: return dummysound()
+	file = os.path.join(main_dir, 'data', file)
+	try:
+		sound = pygame.mixer.Sound(file)
+		return sound
+	except pygame.error:
+		print ('Warning, unable to load, %s' % file)
+	return dummysound()
+	
+#when the root IP is selected, return index.html page
+@app.route('/')
+def index():
 
+	return render_template('index.html', battery = bat_prozent )
+
+@app.route('/<changepin>', methods=['POST'])
+# webserver rerouting - changepin indicates the chosen command which will be decoded and then interpreted with the function chooseAction 
+def reroute(changepin):
+	global ser
+	print(changepin)
+	try:
+		changePin = urllib.unquote(changepin).decode('utf8') #decode changepin to string
+	except:
+		print("changepin is not valid")
+	else:
+		chooseAction(changePin)
+	response = make_response(redirect(url_for('index')))
+	return(response)
+
+# this functions handles input from the gamegui, as well as webserver and BatteryThread-thread
 def chooseAction(data):
-	global bat_prozent
+	global bat_prozent, bat_prozent_lock, move_lock
 	dataArray = data.split(';') # Nachricht wird in ein Array gespeichert
 	print(dataArray)
 	action = dataArray[0] # erstes Argument
@@ -93,25 +124,34 @@ def chooseAction(data):
 		info = dataArray[0]
 		dataArray = dataArray[1:]
 		if info == "status": #wenn status abgefragt wird
-			ser.write("8")
-			bat = str(ser.readline())
-			bat = bat.strip(' \t\n\r')
+			move_lock.acquire()
+			try:
+				ser.write("8")
+				bat = str(ser.readline())
+				bat = bat.strip(' \t\n\r')
+			finally:
+				move_lock.release()
 			try:
 				bat = int(bat)
 			except:
 				print("no data available, recieved message: " + str(bat))
 			else: #weiterverarbeitung des wertes
-				bat_prozent = 0
-				if bat > 242:	
-					bat_prozent = ((bat-242)/4.95)
-					bat_prozent=int(round(bat_prozent))
-				sendtogui("battery;"+str(bat_prozent))
-				print(bat_prozent)
-			print(bat)
+				bat_prozent_lock.acquire()
+				try:
+					bat_prozent = 0
+					if bat > 242:	
+						bat_prozent = ((bat-242)/4.95)
+						bat_prozent=int(round(bat_prozent))
+					sendtogui("battery;"+str(bat_prozent))
+					print(bat_prozent)
+				finally:
+					bat_prozent_lock.release()
 	elif action == "IPcheck":
 		pass
 	
+# this function is called by chooseAction if the robot has to move
 def move(dataArray):
+	global move_lock
 	print(dataArray)
 	dir = dataArray[0]
 	if len(dataArray) > 1: # step informationen vorhanden, Tablet Toucheingabe verwendet
@@ -119,47 +159,100 @@ def move(dataArray):
 		dataArray = dataArray[1:]
 		step = dataArray[0] # fuer spaeter hier Erweiterung moeglich auf Laenge der steps eingehen, jetzt nur standardwert verwendet
 		if dir == "forward":
-			ser.write("7") #zuerst stoppen
-			ser.write("1") #forwardstep
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("1") #forwardstep
+			finally:
+				move_lock.release()
 		elif dir == "backward":
-			ser.write("7") #zuerst stoppen
-			ser.write("2") #backwardstep
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("2") #backwardstep
+			finally:
+				move_lock.release()
 		elif dir == "left":
-			ser.write("7") #zuerst stoppen
-			ser.write("4") #leftstep
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("4") #leftstep
+			finally:
+				move_lock.release()
 		elif dir == "right":
-			ser.write("7") #zuerst stoppen
-			ser.write("3") #rightstep
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("3") #rightstep
+			finally:
+				move_lock.release()
 	else: # keine step informationen vorhanden, daher loop, Joystick verwendet
 		print ("loop")
 		step = "loop"
 		if dir == "forward":
-			ser.write("7") #zuerst stoppen
-			ser.write("c") #forward loop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("c") #forward loop
+			finally:
+				move_lock.release()
 		elif dir == "backward":
-			ser.write("7") #zuerst stoppen
-			ser.write("d") #backward loop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("d") #backward loop
+			finally:
+				move_lock.release()
 		elif dir == "left":
-			ser.write("7") #zuerst stoppen
-			ser.write("f") #leftloop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("f") #leftloop
+			finally:
+				move_lock.release()
 		elif dir == "right":
-			ser.write("7") #zuerst stoppen
-			ser.write("e") #rightloop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("e") #rightloop
+			finally:
+				move_lock.release()
 		elif dir == "forward_right":
-			ser.write("7") #zuerst stoppen
-			ser.write("a") #forward_right loop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("a") #forward_right loop
+			finally:
+				move_lock.release()
 		elif dir == "forward_left":
-			ser.write("7") #zuerst stoppen
-			ser.write("b") #forward_left loop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("b") #forward_left loop
+			finally:
+				move_lock.release()
 		elif dir == "backward_right":
-			ser.write("7") #zuerst stoppen
-			ser.write("5") #backward_right loop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("5") #backward_right loop
+			finally:
+				move_lock.release()
 		elif dir == "backward_left":
-			ser.write("7") #zuerst stoppen
-			ser.write("6") #backward_left loop
+			move_lock.acquire()
+			try:
+				ser.write("7") #zuerst stoppen
+				ser.write("6") #backward_left loop
+			finally:
+				move_lock.release()
 		elif dir == "stop":
-			ser.write("7") #stoppen
+			move_lock.acquire()
+			try:
+				ser.write("7") #stoppen
+			finally:
+				move_lock.release()
 
+# this function is called by chooseAction if the robot has to speak/make a sound			
 def playsound(dataArray):
 	soundname = dataArray[0] #sound(name +) pfad!
 	if len(dataArray) > 1: #random sounds oder Aehnliches
@@ -189,8 +282,9 @@ def playsound(dataArray):
 	else:
 		selectedSound.play()
 
+# this function is called by chooseAction if the facial expression of the robot has to change
 def faceManipulation(dataArray):
-	global radius, eyey, eyex, sadFace, eyestep
+	global radius, eyey, eyex, sadFace, eyestep, move_lock
 	faceObject = dataArray[0] #eyes or smile
 	dataArray = dataArray[1:]
 	if faceObject == "smile":
@@ -235,46 +329,55 @@ def faceManipulation(dataArray):
 			sadFace = 1
 			radius = 0.8
 			print ("serwrite 9")
-			ser.write("9")
+			move_lock.acquire()
+			try:
+				ser.write("9")
+			finally:
+				move_lock.release()
 	if sadFace == 0:
 		DrawFace()
 	elif sadFace == 1:
 		print("vor drawsadface")
 		DrawSadFace()	
 
-#loading sounds from subdir 'data'    
-def load_sound(file):
-	if not pygame.mixer: return dummysound()
-	file = os.path.join(main_dir, 'data', file)
+# Updating Facial expression of robot in case of a happy expression
+def DrawFace():
+	global screen, eyex, eyey, radius
+	screen.fill((0, 0, 0))
+	pygame.draw.circle(screen, (100,250,250), (163,100), 60) #lefteye
+	pygame.draw.circle(screen, (100,250,250), (491,100), 60) #righteye
+	pygame.draw.circle(screen, (10,10,10), (163+eyex,100+eyey), 20) #leftpupil
+	pygame.draw.circle(screen, (10,10,10), (491+eyex,100+eyey), 20) #rightpupil
+	pygame.draw.arc(screen, (100,200,200), (57, -30, 540, 400), 4.7-radius, 4.7+radius, 20) #smile
+	pygame.display.flip()
+
+# Updating Facial expression of robot in case of a sad expression	
+def DrawSadFace():
+	global screen, eyex, eyey, radius
+	screen.fill((0, 0, 0))
+	pygame.draw.circle(screen, (100,250,250), (163,100), 60) #lefteye
+	pygame.draw.circle(screen, (100,250,250), (491,100), 60) #righteye
+	pygame.draw.circle(screen, (10,10,10), (163+eyex,100+eyey), 20) #leftpupil
+	pygame.draw.circle(screen, (10,10,10), (491+eyex,100+eyey), 20) #rightpupil
+	pygame.draw.arc(screen, (100,200,200), (57, 300, 540, 400), 1.57-radius, 1.57+radius, 20) #smile
+	pygame.display.flip()
+
+# This function is used to send information to the gamegui and can be called by the Thread serialRFIDread (for RFID data) and chooseAction (for battery information)
+def sendtogui(i):	
+	global UDP_PORT, SendToApp, IP
+	#IP = '10.10.10.111'
+	if IP == '':
+		return
+	BytesToSend = bytes(":RUN:" + str(i) + ":EOL:")
 	try:
-		sound = pygame.mixer.Sound(file)
-		return sound
-	except pygame.error:
-		print ('Warning, unable to load, %s' % file)
-	return dummysound()
-
-#when the root IP is selected, return index.html page
-@app.route('/')
-def index():
-
-	return render_template('index.html', battery = bat_prozent )
-
-#recieve which pin to change from the button press on index.html
-#each button returns a number that triggers a command in this function
-#
-@app.route('/<changepin>', methods=['POST'])
-def reroute(changepin):
-	global ser
-	print(changepin)
-	try:
-		changePin = urllib.unquote(changepin).decode('utf8') #decode changepin to string
-	except:
-		print("changepin is not valid")
-	else:
-		chooseAction(changePin)
-	response = make_response(redirect(url_for('index')))
-	return(response)
-
+		SendToApp.sendto(BytesToSend, (IP, UDP_PORT+1))
+		print(BytesToSend)
+	except socket.error, msg:
+		print 'Error Code: ' + str(msg[0]) + 'Message ' + msg[1]
+		IP = ''
+		sys.exit()
+		
+# This function is used as Thread to always listen if there is a client (gamegui) sending commands
 def data_listener():
 	global screen, IP, radius, eyex, eyey, sadFace
 	
@@ -324,29 +427,9 @@ def data_listener():
 	finally:
 		pygame.quit()
 
-def DrawFace():
-	global screen, eyex, eyey, radius
-	screen.fill((0, 0, 0))
-	pygame.draw.circle(screen, (100,250,250), (163,100), 60) #lefteye
-	pygame.draw.circle(screen, (100,250,250), (491,100), 60) #righteye
-	pygame.draw.circle(screen, (10,10,10), (163+eyex,100+eyey), 20) #leftpupil
-	pygame.draw.circle(screen, (10,10,10), (491+eyex,100+eyey), 20) #rightpupil
-	pygame.draw.arc(screen, (100,200,200), (57, -30, 540, 400), 4.7-radius, 4.7+radius, 20) #smile
-	pygame.display.flip()
-	
-def DrawSadFace():
-	global screen, eyex, eyey, radius
-	screen.fill((0, 0, 0))
-	pygame.draw.circle(screen, (100,250,250), (163,100), 60) #lefteye
-	pygame.draw.circle(screen, (100,250,250), (491,100), 60) #righteye
-	pygame.draw.circle(screen, (10,10,10), (163+eyex,100+eyey), 20) #leftpupil
-	pygame.draw.circle(screen, (10,10,10), (491+eyex,100+eyey), 20) #rightpupil
-	pygame.draw.arc(screen, (100,200,200), (57, 300, 540, 400), 1.57-radius, 1.57+radius, 20) #smile
-	pygame.display.flip()
-	
-	
+# This function is used as Thread to always listen if there is incoming RFID data
 def serialRFIDread():
-	global readRFIDnumber
+	readRFIDnumber = "empty"
 
 	serRFID=serial.Serial("/dev/ttyUSB0",9600)
 	print('***Serial for RFID reader opened***')
@@ -358,45 +441,32 @@ def serialRFIDread():
 			daten = daten.replace("\x03", "" )
 			daten = daten.replace("\x0a", "" )
 			daten = daten.replace("\x0d", "" )
+			#lock
 			readRFIDnumber=daten
-
+			#release
+			if readRFIDnumber!="empty": #if rfid not locked: if rfid != empty, lock. --- release
+				print('***Serial RFID received:')
+				print(readRFIDnumber)
+				sendtogui("rfid;"+str(readRFIDnumber))
+				readRFIDnumber="empty"	
 	finally:
 		serRFID.close()
 
+# This function is used as Thread to open and handle the webserver
 def webserver():
 	global app
 	# set up the server linstening on port 8765
 	app.run(debug=False, host='0.0.0.0', port=8765) 
 
-def sendtogui(i):	
-	global UDP_PORT, SendToApp, IP
-	#IP = '10.10.10.111'
-	if IP == '':
-		return
-	BytesToSend = bytes(":RUN:" + str(i) + ":EOL:")
-	try:
-		SendToApp.sendto(BytesToSend, (IP, UDP_PORT+1))
-		print(BytesToSend)
-	except socket.error, msg:
-		print 'Error Code: ' + str(msg[0]) + 'Message ' + msg[1]
-		IP = ''
-		sys.exit()	
-		
-
+# This function is used as Thread to periodically update the battery information
 def BatteryInfo():
-	global bat_prozent
-	info = [0, 0, 0]
-	sadFace = 0
-	bat = ""
-	count = 0
 	while True:
-		count += 1
 		chooseAction("battery;status")
 		time.sleep(30)
-		
+	
+# Main function starts all threads and handles event interrupts for closing this program	
 def main():
-	global readRFIDnumber
-
+	
 	AppListener = threading.Thread(target=data_listener)
 	AppListener.daemon = True
 	AppListener.start()
@@ -426,11 +496,6 @@ def main():
 				if event.key == pygame.K_ESCAPE or event.unicode == 'q':
 					pygame.quit()
 					sys.exit()
-			if readRFIDnumber!="empty":
-				print('***Serial RFID received:')
-				print(readRFIDnumber)
-				sendtogui("rfid;"+str(readRFIDnumber))
-				readRFIDnumber="empty"	
 			time.sleep(0.1)
 			
 	finally:
