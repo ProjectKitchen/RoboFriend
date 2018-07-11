3fea43dbda8ce68f70a1a356720b67f0fd1f69bf
Benjamin Klaus <klaus@technikum-wien.at>
2018-07-11 15:44:08 +0200

    added module for accessing ioWarrior for setting ear color and camera position

---
+++ src/Pi/python/ioWarriorModule.py
@@ -0,0 +1,27 @@
+import os
+
+# globals
+cameraPos = 140
+earColorR = 0
+earColorG = 10
+earColorB = 10
+
+def sendToIOWarrior(earColorR, earColorG, earColorB, cameraPos):
+    os.system('sudo ./iowarrior/iow ' + str(earColorR) + ' ' + str(earColorG) + ' ' + str(earColorB) + ' ' + str(cameraPos))
+
+def changeCameraPos(diff):
+    global earColorR, earColorG, earColorB, cameraPos
+    if 10 <= cameraPos + diff <= 160:
+        cameraPos = cameraPos + diff
+        sendToIOWarrior(earColorR, earColorG, earColorB, cameraPos)
+
+def setEarColor(r, g, b):
+    global earColorR, earColorG, earColorB, cameraPos
+    earColorR = r
+    earColorG = g
+    earColorB = b
+    sendToIOWarrior(r, g, b, cameraPos)
+
+#init
+print "initializing ioWarriorModule..."
+sendToIOWarrior(earColorR, earColorG, earColorB, cameraPos)
