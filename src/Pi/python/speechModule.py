27f12faac745c633a6cb38c7d3f01b448d7c053c
Benjamin Klaus <klaus@technikum-wien.at>
2018-07-12 13:29:07 +0200

    added lock to prevent two speech-outputs at the same time

--- src/Pi/python/speechModule.py
+++ src/Pi/python/speechModule.py
@@ -1,17 +1,21 @@
 import pyttsx
+import threading
+
+# globals
+lock = threading.Lock()
+
+def speak(text):
+    global speechEngine, lock
+
+    if not lock.locked():
+        lock.acquire()
+        print "speaking: " + text
+        speechEngine.say(text)
+        speechEngine.runAndWait()
+        lock.release()
 
 #init
 print "initializing speechModule..."
 speechEngine = pyttsx.init()
 speechEngine.setProperty('rate', 150) #150 words per minute
-speechEngine.say('i am robofriend')
-speechEngine.runAndWait()
-
-# globals
-# --- none ---
-
-def speak(text):
-    global speechEngine
-    print "speaking: " + text
-    speechEngine.say(text)
-    speechEngine.runAndWait()
+speak('i am robofriend')
