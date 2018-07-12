b141279de98e129ec5a7e934b10f1c96d1027174
Benjamin Klaus <klaus@technikum-wien.at>
2018-07-12 15:36:25 +0200

    removed legacyApiModule, moved to gameCommunicator, because it implements the API to communicate with the game (tablet)

--- src/Pi/python/gameCommunicator.py
+++ src/Pi/python/gameCommunicator.py
@@ -3,7 +3,11 @@ import threading
 import sys
 
 # own modules
-import legacyApiModule
+import teensyCommunicator
+import faceModule
+import soundModule
+import speechModule
+import gameCommunicator
 
 # globals
 UDP_PORT = 9000 #socket port
@@ -59,10 +63,72 @@ def data_listener():
         receivedData = receivedData[:endFlagIndex] # ab dieser Zeile ist nur noch Befehlskette da (ohne start/endflag)
         IP = addr[0]
         print('***** received data from app (' + str(IP) + ') ******')
-        legacyApiModule.chooseAction(receivedData)
+        chooseAction(receivedData)
 
     # after endless thread-loop stopped
     sock.close()
+
+def chooseAction(data):
+    global currentStatus
+    dataArray = data.split(';') # Nachricht wird in ein Array gespeichert
+    print(dataArray)
+    action = dataArray[0] # erstes Argument
+    dataArray = dataArray[1:] # restliche Argumente
+    if action == "move":
+        move(dataArray)
+    elif action == "say":
+        print('saying :' + dataArray[0])
+        speechModule.speak(dataArray[0])
+    elif action == "sound":
+        info = dataArray[0]
+        dataArray = dataArray[1:]
+        if info == "play":
+            soundModule.playsound(dataArray)
+    elif action == "face":
+        faceModule.faceManipulation(dataArray)
+    elif action == "get":
+        info = dataArray[0]
+        if info == "status" and currentStatus: #wenn status abgefragt wird
+            gameCommunicator.sendtogui("battery;"+str(currentStatus['batVolt']))
+    elif action == "IPcheck":
+        pass
+
+# this function is called by chooseAction if the robot has to move
+def move(dataArray):
+    print(dataArray)
+    dir = dataArray[0]
+    if len(dataArray) > 1: # step informationen vorhanden, Tablet Toucheingabe verwendet
+        print ("step")
+        dataArray = dataArray[1:]
+        step = dataArray[0] # fuer spaeter hier Erweiterung moeglich auf Laenge der steps eingehen, jetzt nur standardwert verwendet
+        if dir == "forward":
+            teensyCommunicator.moveForwardStep()
+        elif dir == "backward":
+            teensyCommunicator.moveBackStep()
+        elif dir == "left":
+            teensyCommunicator.moveLeftStep()
+        elif dir == "right":
+            teensyCommunicator.moveRightStep()
+    else: # keine step informationen vorhanden, daher loop, Joystick verwendet
+        print ("loop")
+        if dir == "forward":
+            teensyCommunicator.moveForwardLoop()
+        elif dir == "backward":
+            teensyCommunicator.moveBackLoop()
+        elif dir == "left":
+            teensyCommunicator.moveLeftLoop()
+        elif dir == "right":
+            teensyCommunicator.moveRightLoop()
+        elif dir == "forward_right":
+            teensyCommunicator.moveForwardRightLoop()
+        elif dir == "forward_left":
+            teensyCommunicator.moveForwardLeftLoop()
+        elif dir == "backward_right":
+            teensyCommunicator.moveBackRightLoop()
+        elif dir == "backward_left":
+            teensyCommunicator.moveBackLeftLoop()
+        elif dir == "stop":
+            teensyCommunicator.stopMovement()
 
 def start():
     global UDP_IP, AppListener, SendToApp, initDone
