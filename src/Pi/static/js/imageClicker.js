0208cb80e1ebfb779cb3e6cb22ee4fb3767f93ae
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-29 16:49:07 +0200

    move forward on middle click, move camera on up/down click on webcam image

--- src/Pi/static/js/imageClicker.js
+++ src/Pi/static/js/imageClicker.js
@@ -1,12 +1,12 @@
 function ImageClicker() {
     var thiz = this;
 
-    thiz.webcamClicked = function(event) {
+    thiz.webcamClicked = function (event) {
         var rect = L('#webcamPlaceholder').getBoundingClientRect();
         var x = event.x - rect.left;
         var y = event.y - rect.top;
-        var px = (x/rect.width) * 100;
-        var py = (y/rect.height) * 100;
+        var px = (x / rect.width) * 100;
+        var py = (y / rect.height) * 100;
 
         var isLeft = px < 30;
         var isRight = px > 70;
@@ -14,11 +14,16 @@ function ImageClicker() {
         var isDown = py > 70;
         var isMiddleY = !isUp && !isDown;
         var isMiddleX = !isLeft && !isRight;
-        if(isMiddleY && isLeft){
-            communicator.sendMoveXY(-50,50,1);
-        }
-        if(isMiddleY && isRight){
-            communicator.sendMoveXY(50,-50,1);
+        if (isMiddleY && isLeft) {
+            communicator.sendMoveXY(-50, 50, 1);
+        } else if (isMiddleY && isRight) {
+            communicator.sendMoveXY(50, -50, 1);
+        } else if (isMiddleX && isMiddleY) {
+            communicator.sendMoveXY(50, 50, 1);
+        } else if (isMiddleX && isUp) {
+            communicator.sendAction('camera/up')
+        } else if (isMiddleX && isDown) {
+            communicator.sendAction('camera/down')
         }
     };
 }
