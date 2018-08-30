113a9ebcd74292d279df61b2ec09103622cfd52d
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-30 13:30:55 +0200

    added moving average for battery value, changed battery thresholds

--- src/Pi/python/statusModule.py
+++ src/Pi/python/statusModule.py
@@ -20,6 +20,7 @@ keyScreenshotTimestamp = 'screenshotTimestamp'
 screenshotTimestamp = None
 runFlag = True
 batCoversionConstant =  0.0480769
+batMovingAverageN = 20
 
 def getStatus():
     return currentStatus
@@ -59,10 +60,10 @@ def StatusInfo():
         gameCommunicator.sendtogui("battery;"+str(getBatteryVoltage()))
 
         print ("Battery= " + str(getBatteryVoltage()) + " Volt)")
-        if getBatteryVoltage() < 11.0:
+        if getBatteryVoltage() < 12.0:
             speechModule.speakBatteryLow()
-            print ("LOW BATTERY - Please Recharge! Robofriend will shutdown automatically at 10.5 Volt!")
-        if getBatteryVoltage() < 10.5:
+            print ("LOW BATTERY - Please Recharge! Robofriend will shutdown automatically at 11.8 Volt!")
+        if getBatteryVoltage() < 11.8:
             speechModule.speakBatteryShutdown()
             sleep(5)
             os.system("init 0")
@@ -70,16 +71,17 @@ def StatusInfo():
         time.sleep(refreshIntervalMs / 1000.0)
 
 def updateFromRawStatus(rawStatus):
-    global currentStatus, keyBat, keyIrL, keyIrM, keyIrR, batCoversionConstant
+    global currentStatus, keyBat, keyIrL, keyIrM, keyIrR, batCoversionConstant, batMovingAverageN
 
-    batVolt = -1
+    batVolt = getBatteryVoltage()
     irSensorLeft = -1
     irSensorMiddle = -1
     irSensorRight = -1
     try:
         statusArray = rawStatus.split(',')
         if (statusArray[0] == "Sensors"):
-            batVolt = int(statusArray[1]) * batCoversionConstant
+            newbatVolt = int(statusArray[1]) * batCoversionConstant
+            batVolt = getMovingAverage(newbatVolt, batVolt, batMovingAverageN)
             irSensorLeft = int(statusArray[2])
             irSensorMiddle = int(statusArray[3])
             irSensorRight = int(statusArray[4])
@@ -91,6 +93,13 @@ def updateFromRawStatus(rawStatus):
     currentStatus[keyIrM] = irSensorMiddle
     currentStatus[keyIrR] = irSensorRight
     return currentStatus
+
+def getMovingAverage(newValue, currentMean, n):
+    if not currentMean:
+        return newValue
+    newMean = currentMean - currentMean / n
+    newMean = newMean + newValue / n
+    return newMean
 
 def start():
     global StatusThread
