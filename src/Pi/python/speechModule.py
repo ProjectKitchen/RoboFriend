c6accfe9e81f911a8e20ebd29ee95199e66a195e
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-30 14:17:31 +0200

    only do update lastSpeakTimestamp if it was a auto-speak

--- src/Pi/python/speechModule.py
+++ src/Pi/python/speechModule.py
@@ -10,20 +10,23 @@ def speak(text, minPause = 0):
     global speechEngine, wordRate, lastSpeakTimestamp
     if time.time() - lastSpeakTimestamp > minPause:
         print "speaking: " + text
-        lastSpeakTimestamp = time.time()
         speechEngine = pyttsx.init()
         speechEngine.setProperty('rate', wordRate)
         speechEngine.say(text)
         speechEngine.runAndWait()
 
 def speakBatteryLow():
+    global lastSpeakTimestamp
     minimumPause = 30 #minimum pause of 30 seconds
     possibleTexts = ['Please recharge me', 'I am tired', 'My energy is running low', 'I am feeling exhausted', 'Do you have some energy for me?', 'I am hungry']
     speak(random.choice(possibleTexts), minimumPause)
+    lastSpeakTimestamp = time.time()
 
 def speakBatteryShutdown():
+    global lastSpeakTimestamp
     possibleTexts = ['I am tired. I have to go to sleep. Bye bye.', 'My energy is too low. Bye bye.']
     speak(random.choice(possibleTexts))
+    lastSpeakTimestamp = time.time()
 
 
 #init
