d88fe35716f4d631509d859febac27c42152af84
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-30 14:27:51 +0200

    moved minimum pause to battery low method

--- src/Pi/python/speechModule.py
+++ src/Pi/python/speechModule.py
@@ -6,21 +6,21 @@ import time
 lastSpeakTimestamp = time.time()
 wordRate = 150 #words per minute
 
-def speak(text, minPause = 0):
-    global speechEngine, wordRate, lastSpeakTimestamp
-    if time.time() - lastSpeakTimestamp > minPause:
-        print "speaking: " + text
-        speechEngine = pyttsx.init()
-        speechEngine.setProperty('rate', wordRate)
-        speechEngine.say(text)
-        speechEngine.runAndWait()
+def speak(text):
+    global speechEngine, wordRate
+    print "speaking: " + text
+    speechEngine = pyttsx.init()
+    speechEngine.setProperty('rate', wordRate)
+    speechEngine.say(text)
+    speechEngine.runAndWait()
 
 def speakBatteryLow():
     global lastSpeakTimestamp
     minimumPause = 30 #minimum pause of 30 seconds
     possibleTexts = ['Please recharge me', 'I am tired', 'My energy is running low', 'I am feeling exhausted', 'Do you have some energy for me?', 'I am hungry']
-    speak(random.choice(possibleTexts), minimumPause)
-    lastSpeakTimestamp = time.time()
+    if time.time() - lastSpeakTimestamp > minimumPause:
+        lastSpeakTimestamp = time.time()
+        speak(random.choice(possibleTexts))
 
 def speakOnRecharge():
     possibleTexts = ['Oh. Thank you very much!', 'Ah. This is good!', 'I am feeling refreshed.', 'Yeah. This is engergy.']
