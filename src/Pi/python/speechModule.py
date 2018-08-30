55a3d2438208cde2210c9f96986454f23112d7f1
Benjamin Klaus <klaus@technikum-wien.at>
2018-08-30 12:11:30 +0200

    added speak methods for battery state

--- src/Pi/python/speechModule.py
+++ src/Pi/python/speechModule.py
@@ -1,15 +1,29 @@
 import pyttsx
+import random
+import time
 
 # globals
-# --- none ---
+lastSpeakTimestamp = None
+wordRate = 150 #words per minute
 
-def speak(text):
-    global speechEngine
-    print "speaking: " + text
-    speechEngine = pyttsx.init()
-    speechEngine.setProperty('rate', 150) #150 words per minute
-    speechEngine.say(text)
-    speechEngine.runAndWait()
+def speak(text, minPause = 0):
+    global speechEngine, wordRate, lastSpeakTimestamp
+    if lastSpeakTimestamp and time.time() - lastSpeakTimestamp > minPause:
+        print "speaking: " + text
+        speechEngine = pyttsx.init()
+        speechEngine.setProperty('rate', wordRate)
+        speechEngine.say(text)
+        speechEngine.runAndWait()
+
+def speakBatteryLow():
+    minimumPause = 30 #minimum pause of 30 seconds
+    possibleTexts = ['Please recharge me', 'I am tired', 'My energy is running low', 'I am feeling exhausted', 'Do you have some energy for me?', 'I am hungry']
+    speak(random.choice(possibleTexts), minimumPause)
+
+def speakBatteryShutdown():
+    possibleTexts = ['I am tired. I have to go to sleep. Bye bye.', 'My energy is too low. Bye bye.']
+    speak(random.choice(possibleTexts))
+
 
 #init
 print "initializing speechModule..."
