12431b5b95aa9cbf401b2d8ae0b1102d352c4db3
Benjamin Klaus <klaus@technikum-wien.at>
2018-09-04 12:11:23 +0200

    added i18n, added method to speak random texts

--- src/Pi/python/speechModule.py
+++ src/Pi/python/speechModule.py
@@ -21,21 +21,44 @@ def speak(text):
     speechEngine.say(text)
     speechEngine.runAndWait()
 
+def speakRandom():
+    global lastSpeakTimestamp, language
+    possibleTexts = {
+        'english': ['Hello', 'Hi', 'Hello, how are you?', 'I am fine. How are you?', 'Do you like a snack?', 'Do you like to be my friend?'],
+        'german' : ['Hallo', 'Guten Tag', 'Hallo, wie gehts?', 'Mir geht es gut. Wie geht es dir?', 'Möchtest du einen Sneck?', 'Willst du mein Freund sein?']
+    }
+    possibleInLang = possibleTexts[language]
+    speak(random.choice(possibleInLang))
+
 def speakBatteryLow():
-    global lastSpeakTimestamp
+    global lastSpeakTimestamp, language
     minimumPause = 30 #minimum pause of 30 seconds
-    possibleTexts = ['Please recharge me', 'I am tired', 'My energy is running low', 'I am feeling exhausted', 'Do you have some energy for me?', 'I am hungry']
     if time.time() - lastSpeakTimestamp > minimumPause:
+        possibleTexts = {
+            'english': ['Please recharge me', 'I am tired', 'My energy is running low', 'I am feeling exhausted', 'Do you have some energy for me?', 'I am hungry'],
+            'german' : ['Bitte lade mich auf', 'Ich bin müde', 'Meine Energie neigt sich dem Ende zu', 'Ich fühle mich erschöpft', 'Hast du ein bisschen Energie für mich?', 'Ich habe Hunger']
+        }
+        possibleInLang = possibleTexts[language]
         lastSpeakTimestamp = time.time()
-        speak(random.choice(possibleTexts))
+        speak(random.choice(possibleInLang))
 
 def speakOnRecharge():
-    possibleTexts = ['Oh. Thank you very much!', 'Ah. This is good!', 'I am feeling refreshed.', 'Yeah. This is engergy.']
-    speak(random.choice(possibleTexts))
+    global lastSpeakTimestamp, language
+    possibleTexts = {
+        'english': ['Thank you for recharging me!', 'I feel the engery', 'I am feeling refreshed.'],
+        'german' : ['Danke fürs Aufladen!', 'Ich fühle die Energie', 'Ich fühle mich erfrischt']
+    }
+    possibleInLang = possibleTexts[language]
+    speak(random.choice(possibleInLang))
 
 def speakBatteryShutdown():
-    possibleTexts = ['I am tired. I have to go to sleep. Bye bye.', 'My energy is too low. Bye bye.']
-    speak(random.choice(possibleTexts))
+    global lastSpeakTimestamp, language
+    possibleTexts = {
+        'english': ['I am tired. I have to go to sleep. Bye bye.', 'My energy is too low. Bye bye.'],
+        'german' : ['Ich bin müde uns muss schlafen gehen.... Tschüss.', 'Meine Energie ist zu niedrig.... Tschüss.']
+    }
+    possibleInLang = possibleTexts[language]
+    speak(random.choice(possibleInLang))
 
 
 #init
