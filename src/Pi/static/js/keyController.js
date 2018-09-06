e788351d60982269d1a8b6299c5415ef7b8c103a
Benjamin Klaus <klaus@technikum-wien.at>
2018-09-06 13:43:29 +0200

    control move with WASD instead of arrows, use ESC to stop moving

--- src/Pi/static/js/keyController.js
+++ src/Pi/static/js/keyController.js
@@ -1,5 +1,10 @@
 function KeyController() {
     var thiz = this;
+    thiz.keyCodeUp = 87; //W
+    thiz.keyCodeDown = 83; //A
+    thiz.keyCodeLeft = 65;//S
+    thiz.keyCoderight = 68; //D
+
     thiz.lastMove = new Date().getTime();
     thiz.movePause = 200;
     thiz.pressedLeft = false;
@@ -11,49 +16,62 @@ function KeyController() {
     thiz.init = function () {
         document.addEventListener('keydown', function (event) {
             switch (event.keyCode) {
-                case 38: //UP
+                case thiz.keyCodeUp:
                     thiz.moving = true;
                     thiz.pressedUp = true;
                     sendMoveInternal(event);
                     break;
-                case 40: //DOWN
+                case thiz.keyCodeDown:
                     thiz.moving = true;
                     thiz.pressedDown = true;
                     sendMoveInternal(event);
                     break;
-                case 37: //LEFT
+                case thiz.keyCodeLeft:
                     thiz.moving = true;
                     thiz.pressedLeft = true;
                     sendMoveInternal(event);
                     break;
-                case 39: //RIGHT
+                case thiz.keyCoderight:
                     thiz.moving = true;
                     thiz.pressedRight = true;
                     sendMoveInternal(event);
+                    break;
+                case 27: //ESC
+                    event.preventDefault();
+                    stopMoving();
+                    imageClicker.stop();
                     break;
             }
         });
         document.addEventListener('keyup', function (event) {
             switch (event.keyCode) {
-                case 38: //UP
+                case thiz.keyCodeUp:
                     thiz.pressedUp = false;
                     break;
-                case 40: //DOWN
+                case thiz.keyCodeDown:
                     thiz.pressedDown = false;
                     break;
-                case 37: //LEFT
+                case thiz.keyCodeLeft:
                     thiz.pressedLeft = false;
                     break;
-                case 39: //RIGHT
+                case thiz.keyCoderight:
                     thiz.pressedRight = false;
                     break;
             }
             if (!thiz.pressedUp && !thiz.pressedDown && !thiz.pressedLeft && !thiz.pressedRight) {
-                communicator.moveStop();
-                thiz.moving = false;
+                stopMoving();
             }
         });
     };
+
+    function stopMoving() {
+        thiz.moving = false;
+        thiz.pressedUp = false;
+        thiz.pressedDown = false;
+        thiz.pressedLeft = false;
+        thiz.pressedRight = false;
+        communicator.moveStop();
+    }
 
     function sendMoveInternal(event) {
         event.preventDefault();
