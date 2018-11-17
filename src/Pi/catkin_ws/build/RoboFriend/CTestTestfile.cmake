8728711ef987320a08fbacbc75dc486af930753c
momokarl <m.karlitepe@hotmail.com>
2018-11-17 15:21:42 +0100

    ROS workspace and robofriend package created

---
+++ src/Pi/catkin_ws/build/RoboFriend/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/muhammed/Okul/ESP/RoboFriend/src/Pi/catkin_ws/src/RoboFriend
+# Build directory: /home/muhammed/Okul/ESP/RoboFriend/src/Pi/catkin_ws/build/RoboFriend
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/Pi/catkin_ws/build/robofriend/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/muhammed/Okul/ESP/RoboFriend/src/Pi/catkin_ws/src/robofriend
+# Build directory: /home/muhammed/Okul/ESP/RoboFriend/src/Pi/catkin_ws/build/robofriend
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
