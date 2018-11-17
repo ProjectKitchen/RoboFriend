fbb6528448ad3d7d795be526ffbf1f5a5de0418a
mzahedi <martinzahedi@gmail.com>
2018-11-17 13:48:43 +0100

    something went wrong, merge start

--- src/Pi/start_robofriend.sh
+++
@@ -1,6 +0,0 @@
-#cd mjpg
-mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 320 -y 240 -quality 30 --fps 15 -vf" &
-#..cd 
-python3 robofriend.py
-
-
--- src/Teensy/Parser.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARSER_H_
-#define PARSER_H_
-
-void parser_init();
-void parser_processSerialCommands();
-
-#endif /* PARSER_H_ */
