eb3ef5d18fa7e1164e20328e2a506b3ba22c1995
momokarl <m.karlitepe@hotmail.com>
2018-11-24 14:24:18 +0100

    Rework of origin rfid reader to get ROS Compatible - ros publisher added - message protocol added - queue to ensure communication between threads added

---
+++ src/Pi/python/ros_rfid_node.py
@@ -0,0 +1,74 @@
+import serial
+import threading
+import gameCommunicator
+from std_msgs import String
+
+# global variables
+runFlag = True
+
+def node_stop():
+    global runFlag
+    runFlag = False
+
+def node_start():
+    global runFlag
+
+    received_message = ""
+
+    print("[INFO] ROS RFID Node started!\n")
+
+    pub = rospy.Publisher('rfid_topic', String, queue_size = 10)
+    rospy.init('rfid_node', anonymous = True)
+
+    # queue to ensure a communication system between rfid thread and publisher
+    thread_queue = queue.Queue()
+
+    try:
+        serial_rfid = serial.Serial("/dev/ttyUSB0", 9600)
+        print("[INFO] Serial for RFID reader opened!")
+
+        # thread to handle the rfid date
+        rfid_thread = threading.Thread(
+            target = serial_rfid_read,
+            args = (serial_rfid, thread_queue, )
+        )
+
+        # start rfid thread
+        rfid_thread.start()
+
+        while runFlag:
+            received_messsage = thread_queue.get()
+            transmit_message = message_merge(received_message)
+            print("[INFO] Transmitted data from rfid node: {}".format(transmit_message))
+            game.Communicator(transmit_message))
+            pub.publish(transmit_message)
+    except:
+        print("[INFO] Serial for RFID could not be opened!!!")
+
+        if runFlag == False:
+            serial_rfid.close()
+
+def serial_rfid_read(serial, queue):
+    global runFlag
+
+    data = ""
+    readRFIDnumber = None
+
+    try:
+        while runFlag:
+            data = serial.read(16)
+            data = data.replace("\x02", "" )
+            data = data.replace("\x03", "" )
+            data = data.replace("\x0a", "" )
+            data = data.replace("\x0d", "" )
+            readRFIDnumber = data
+
+            if readRFIDnumber != "empty":
+                print("[INFO] Serial rfid received: {}".format(readRFIDnumber))
+                queue.put(readRFIDnumber)
+                readRFIDnumber = "empty"
+    finally:
+        serial.close()
+
+def message_merge(command):
+    return "rfid;" + command
