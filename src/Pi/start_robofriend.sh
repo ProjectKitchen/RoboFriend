03c8bcc50eaba6d62ef25c99991472d68a3ca3b4
Muhammed Karlitepe <m.karlitepe@hotmail.com>
2018-12-07 11:27:47 +0000

    rename of main script due to the changes according ros package

--- src/Pi/start_robofriend.sh
+++ src/Pi/start_robofriend.sh
@@ -2,5 +2,5 @@
 mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 320 -y 240 -quality 30 --fps 15" &
 #cd .. 
 source /home/pi/.bashrc
-python3 robofriend1.py
+python3 robofriend.py
 
