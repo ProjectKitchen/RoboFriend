c2635864f703342e0b0fce1c1d8a1a3951fa7678
momokarl <m.karlitepe@hotmail.com>
2018-12-16 11:13:39 +0100

    Publisher from brain node to teensy-node to control the motors added

--- src/Pi/python/RobobrainPublisherHandler.py
+++ src/Pi/python/RobobrainPublisherHandler.py
@@ -2,14 +2,23 @@ import rospy
 
 #import ros message
 from ros_robofriend.msg import SpeechData
+from ros_robofriend.msg import TeensyMotorData
 
 class RobobrainPublisherHandler():
     def __init__(self):
         self._speech_pub = rospy.Publisher('T_SPEECH_DATA', SpeechData, queue_size = 10)
+        self._teensy_motor_pub = rospy.Publisher('T_TEENSY_MOTOR_DATA', TeensyMotorData, queue_size = 10)
         self._speech_msg = SpeechData()
+        self._teensy_motor_msg = TeensyMotorData()
 
     def speech_message_publish(self, mode, text = None):
         self._speech_msg.mode = mode
         self._speech_msg.text = text
         self._speech_pub.publish(self._speech_msg)
         print("[INFO] {} - Speech Published Data: {}".format(self.__class__.__name__, self._speech_msg))
+
+    def teensy_motor_message_publish(self, direction, duration):
+        self._teensy_motor_msg.duration = duration
+        self._teensy_motor_msg.direction = direction
+        self._teensy_motor_pub.publish(self._teensy_motor_msg)
+        print("[INFO] {} - Teensy Motor Published Data: {}\n".format(self.__class__.__name__, self._teensy_motor_msg))
