cc63d4838e1abdbdf3233f6c97bdb5c24540485a
momokarl <m.karlitepe@hotmail.com>
2018-12-27 15:14:30 +0100

    logic to interact in IDLE state added

--- src/Pi/python/ROS_Node/RobobrainNode/RobobrainStateHandler.py
+++ src/Pi/python/ROS_Node/RobobrainNode/RobobrainStateHandler.py
@@ -10,11 +10,15 @@ class RobobrainStateHandler():
         'MANUAL' : 4
     }
 
-    def __init__(self):
+    idle_elapse_time = 90         # waits 90 sec to change state if no input from webserver and keyboard
+
+    def __init__(self, event):
         self.__state = RobobrainStateHandler.robostate["IDLE"]
         self.__lock = Lock()
+        self.__start_thread()
+        self.__event = event
 
-    def start_thread(self):
+    def __start_thread(self):
         thread = Thread(target = self.__state_hander_thread)
         thread.daemon = True
         thread.start()
@@ -22,15 +26,19 @@ class RobobrainStateHandler():
     def __state_hander_thread(self):
         print("{} - Thread to handle the states started!".format(__class__.__name__))
         while True:
-            print("Thread is ongoing")
-            sleep(3)
-
-
+            if self.__state == RobobrainStateHandler.robostate["IDLE"]:
+                print("{} - Waiting for input or timeout within {} seconds".format(__class__.__name__, RobobrainStateHandler.idle_elapse_time))
+                event_is_set = self.__event.wait(RobobrainStateHandler.idle_elapse_time)
+                if event_is_set == True:
+                    print("Input from either keyboard or webserver therefore stay in IDLE state!")
+                    self.__event.clear()
+                else:
+                    print("No input within {} sec therefore change state!".format(RobobrainStateHandler.idle_elapse_time))
 
     @property
     def state(self):
+        self.__lock_acquire()
         retVal = None
-        self.__lock_aquire()
         retVal = self.__state
         self.__lock_release()
         return retVal
@@ -41,10 +49,10 @@ class RobobrainStateHandler():
         self.__state = value
         self.__lock_release()
 
-    def __lock_aquire():
+    def __lock_acquire(self):
         print("Lock aquired!")
-        self.__lock.aquire()
+        self.__lock.acquire()
 
-    def __lock_release():
+    def __lock_release(self):
         print("Lock released!")
         self.__lock.release()
