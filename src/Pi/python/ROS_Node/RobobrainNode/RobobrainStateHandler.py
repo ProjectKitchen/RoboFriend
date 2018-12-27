b2e30e4fe9ed650c4f8fb78d0d23c51d580a4750
momokarl <m.karlitepe@hotmail.com>
2018-12-27 16:35:16 +0100

    state handler when reaching critical battery level added

--- src/Pi/python/ROS_Node/RobobrainNode/RobobrainStateHandler.py
+++ src/Pi/python/ROS_Node/RobobrainNode/RobobrainStateHandler.py
@@ -6,8 +6,8 @@ class RobobrainStateHandler():
     robostate = {
         'IDLE' : 1, \
         'FIND_CHARGING_STATION' : 2, \
-        'AUTONOM' : 3, \
-        'MANUAL' : 4
+        'FACEDETECTION' : 3, \
+        'SHUTDOWN' : 4
     }
 
     idle_elapse_time = 90         # waits 90 sec to change state if no input from webserver and keyboard
@@ -26,14 +26,27 @@ class RobobrainStateHandler():
     def __state_hander_thread(self):
         print("{} - Thread to handle the states started!".format(__class__.__name__))
         while True:
-            if self.__state == RobobrainStateHandler.robostate["IDLE"]:
-                print("{} - Waiting for input or timeout within {} seconds".format(__class__.__name__, RobobrainStateHandler.idle_elapse_time))
+            if self.state == RobobrainStateHandler.robostate["SHUTDOWN"]:
+                print("[INFO] Within SHUTDOWN state")
+                #TODO: Speech module publishen
+                sleep(5)
+                os.system("init 0")
+            elif self.state ==RobobrainStateHandler.robostate["FIND_CHARGING_STATION"]:
+                print("[INFO] FIND_CHARGING_STATION state")
+                sleep(5)
+            elif self.state == RobobrainStateHandler.robostate["IDLE"]:
+                print("[INFO] Within IDLE state")
                 event_is_set = self.__event.wait(RobobrainStateHandler.idle_elapse_time)
                 if event_is_set == True:
                     print("Input from either keyboard or webserver therefore stay in IDLE state!")
                     self.__event.clear()
                 else:
-                    print("No input within {} sec therefore change state!".format(RobobrainStateHandler.idle_elapse_time))
+                    print("No input within {} sec therefore change state to FACEDETECTION state!".format(RobobrainStateHandler.idle_elapse_time))
+                    self.__state = RobobrainStateHandler.robostate["FACEDETECTION"]
+            elif self.state == RobobrainStateHandler.robostate["FACEDETECTION"]:
+                print("[INFO] Within FACEDETECTION state")
+                sleep(5)
+                #TODO: start facedetection process
 
     @property
     def state(self):
@@ -45,14 +58,14 @@ class RobobrainStateHandler():
 
     @state.setter
     def state(self, value):
-        self.__lock_aquire()
+        self.__lock_acquire()
         self.__state = value
         self.__lock_release()
 
     def __lock_acquire(self):
-        print("Lock aquired!")
+        #print("Lock aquired!")
         self.__lock.acquire()
 
     def __lock_release(self):
-        print("Lock released!")
+        # print("Lock released!")
         self.__lock.release()
