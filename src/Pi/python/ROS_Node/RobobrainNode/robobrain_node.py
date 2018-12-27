6e96f644aabd8834a53f8fa1380f309ddea9f6f1
momokarl <m.karlitepe@hotmail.com>
2018-12-27 16:35:34 +0100

    Minior changes

--- src/Pi/python/ROS_Node/RobobrainNode/robobrain_node.py
+++ src/Pi/python/ROS_Node/RobobrainNode/robobrain_node.py
@@ -64,7 +64,7 @@ def RobobrainHandler():
     robostate = RobobrainStateHandler(event)         # sets actual state to IDLE and starts thread
     keyboard = RobobrainKeyboardDataHandler(event, robostate)
     facedetection = RobobrainFacedetectionDataHandler()
-    battery_infrared = RobobrainBatteryInfraredDataHandler()
+    battery_infrared = RobobrainBatteryInfraredDataHandler(robostate)
     rospy.Subscriber(topics['T_KEYB_DATA'], KeyboardData, keyboard_data_cb, keyboard)
     rospy.Subscriber(topics['T_CAM_DATA'], CamData, facedetection_data_cb, facedetection)
     rospy.Subscriber(topics['T_BAT_INF_DATA'], BatInfMsgData, battery_infrared_data_cb, battery_infrared)
