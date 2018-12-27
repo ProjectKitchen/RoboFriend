4ad46a264081883f0a9f57e12b5c26559346f4ef
momokarl <m.karlitepe@hotmail.com>
2018-12-27 15:12:58 +0100

    first logic to handle input data from keayboard node in IDLE state added

--- src/Pi/python/ROS_Node/RobobrainNode/RobobrainKeyboardDataHandler.py
+++ src/Pi/python/ROS_Node/RobobrainNode/RobobrainKeyboardDataHandler.py
@@ -1,16 +1,30 @@
+from ROS_Node.RobobrainNode.RobobrainStateHandler import *
+
 
 class RobobrainKeyboardDataHandler():
 
-    def __init__(self):
+    def __init__(self, event, robostate):
         self.__quit = None
         self.__up_down = None
         self.__pressed_key = None
+
+        self.__event = event
+        self.__robostate_obj = robostate
 
     def process_data(self, data):
         self.__quit = data.quit
         self.__up_down = data.up_down
         self.__pressed_key = data.pressed_key
         print("[INFO] Class: {} ... Received message from keyboard-node: {}\n".format(self.__class__.__name__, data))
+        self.__input_handler(data)
+
+    def __input_handler(self, data):
+        print("[INFO] Within keyboard input handler!")
+        print("[INFO] actual State: {}".format(self.__robostate_obj.state))
+        if self.__robostate_obj.state == RobobrainStateHandler.robostate["IDLE"]:
+            self.__event.set()
+            print("[INFO] Event set!")
+            #TODO: Input processing according to the IDLE State
 
     # command
     @property
