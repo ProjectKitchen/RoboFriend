213decff945b6acdf2b107ce2cdf2d0b47b2fd79
momokarl <m.karlitepe@hotmail.com>
2018-12-29 11:52:01 +0100

    implement ears_led_node/EarsLedDataHandler to handle led of Robofriend and process incoming messages from robobrain node

---
+++ src/Pi/python/ROS_Node/EearsLedNode/ears_led_node.py
@@ -0,0 +1,27 @@
+import threading
+import rospy
+
+# import ros message
+#from ros_robofriend.msg import EarsLedData
+
+# import ros module
+from ROS_Node.EearsLedNode.EarsLedDataHandler import *
+
+def node_start():
+    print("[INFO] ROS Ears/Led Node started!\n")
+
+    # thread to handle incoming messages from brain node
+    ears_led_thread = threading.Thread(
+        target = handle_ears_led
+    )
+
+    # start thread as a daeomon
+    ears_led_thread.daemon = True
+
+    # start ears/led thread
+    ears_led_thread.start()
+
+def handle_ears_led():
+
+    print("[INFO] Thread for ears_led started !")
+    ears_led = EarsLedDataHandler()
