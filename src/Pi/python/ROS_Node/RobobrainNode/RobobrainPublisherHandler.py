3acc26a95ea8fa8e01455735beb83acb8c152c46
momokarl <m.karlitepe@hotmail.com>
2018-12-30 17:30:57 +0100

    implement pubisher to communicate with ears_led_node

--- src/Pi/python/ROS_Node/RobobrainNode/RobobrainPublisherHandler.py
+++ src/Pi/python/ROS_Node/RobobrainNode/RobobrainPublisherHandler.py
@@ -3,6 +3,7 @@ import rospy
 #import ros message
 from ros_robofriend.msg import SpeechData
 from ros_robofriend.msg import TeensyMotorData
+from ros_robofriend.msg import EarsLedData
 
 class RobobrainPublisherHandler():
 
@@ -10,8 +11,10 @@ class RobobrainPublisherHandler():
         self._topics = topics
         self._speech_pub = rospy.Publisher(topics['T_SPEECH_DATA'], SpeechData, queue_size = 10)
         self._teensy_motor_pub = rospy.Publisher(topics['T_TEENSY_MOTOR_DATA'], TeensyMotorData, queue_size = 10)
+        self._ears_led_pub = rospy.Publisher(topics['T_EARS_LED_DATA'], EarsLedData, queue_size = 10)
         self._speech_msg = SpeechData()
         self._teensy_motor_msg = TeensyMotorData()
+        self._ears_led_msg = EarsLedData()
 
     def speech_message_publish(self, mode, text = None):
         self._speech_msg.mode = mode
@@ -24,3 +27,10 @@ class RobobrainPublisherHandler():
         self._teensy_motor_msg.direction = direction
         self._teensy_motor_pub.publish(self._teensy_motor_msg)
         print("[INFO] {} - Teensy Motor Published Data: {}\n".format(self.__class__.__name__, self._teensy_motor_msg))
+
+    def ears_led_message_publish(self, random, repeat_num, rgb_color):
+        self._ears_led_msg.random = random
+        self._ears_led_msg.repeat_num = repeat_num
+        self._ears_led_msg.rgb_color = rgb_color
+        self._ears_led_pub.publish(self._ears_led_msg)
+        print("[INFO] {} - Ears/Led Published Data: {}\n".format(self.__class__.__name__, self._ears_led_msg))
