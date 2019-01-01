c1a58cb7ec58000519b03932c2666bd15c9a376e
momokarl <m.karlitepe@hotmail.com>
2019-01-01 14:10:34 +0100

    Implement sucbscriber to enable a communication with brain node

---
+++ src/Pi/python/ROS_Node/ServoCamNode/servo_cam_node.py
@@ -0,0 +1,10 @@
+import rospy
+
+# import ros message
+from std_msgs.msg import Int8
+
+def node_start():
+    print("[INFO] ROS Servo Camera Node started!\n")
+
+    servocam = ServoCamDataHandler()
+    rospy.Subscriber("T_SERVO_CAM_DATA", Int8, servocam.process_data)
