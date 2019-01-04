255820d377f71044f208f9ee4e2a4fcf1529ef17
mzahedi <martinzahedi@gmail.com>
2019-01-04 11:13:39 +0100

    writing new robofriend.py module based on ROS - instable version yet

--- src/Pi/python/systemModule.py
+++ src/Pi/python/systemModule.py
@@ -1,8 +1,14 @@
 # external modules
 import os
 import time
+
 import subprocess
+import shlex
+import sys
 import signal
+import psutil
+import roslaunch
+
 
 # own modules
 import speechModule
@@ -15,16 +21,60 @@ def shutdown():
     time.sleep(3)
     os.system('sudo init 0')
 
-def roscore_start():
-    global roscore_pid
 
-    roscore = subprocess.Popen('roscore')
-    roscore_pid = roscore.pid
-    print("[INFO] Roscore started")
+def kill_child_processes(parent_pid, sig=signal.SIGTERM):
+    try:
+        parent = psutil.Process(parent_pid)
+        print(parent)
+    except psutil.NoSuchProcess:
+        print("parent process not existing")
+        return
+    children = parent.children(recursive=True)
+    print(children)
+    for process in children:
+        print("try to kill child: " + str(process))
+        process.send_signal(sig)
 
-def roscore_terminate():
-    if roscore_pid != 0:
-        os.kill(roscore_pid, signal.SIGTERM)
-        print("[INFO] Roscore terminated!")
-    else:
-        pass
+class Roscore(object):
+    """
+    roscore wrapped into a subprocess.
+    Singleton implementation prevents from creating more than one instance.
+    """
+    __initialized = False
+    def __init__(self):
+        if Roscore.__initialized:
+            raise Exception("You can't create more than 1 instance of Roscore.")
+        Roscore.__initialized = True
+    def run(self):
+        try:
+            self.roscore_process = subprocess.Popen(['roscore'])
+            self.roscore_pid = self.roscore_process.pid  # pid of the roscore process (which has child processes)
+        except OSError as e:
+            sys.stderr.write('roscore could not be launched')
+            raise e
+    def terminate(self):
+        print("try to kill child pids of roscore pid: " + str(self.roscore_pid))
+        kill_child_processes(self.roscore_pid)
+        self.roscore_process.terminate()
+        self.roscore_process.wait()  # important to prevent from zombie process
+        Roscore.__initialized = False
+
+class RosRobo(object):
+
+    __initialized = False
+    def __init__(self):
+        if RosRobo.__initialized:
+            raise Exception("You can't create more than 1 instance of RosRobo.")
+        RosRobo.__initialized = True
+    def run(self):
+        try:
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(
+                uuid,
+                ["catkin_ws/src/robofriend/launch/robofriend_startup.launch"]
+                )
+            launch.start()
+        except Exception, OSError as e:
+            sys.stderr.write('rosrobo could not be launched')
+            raise e
--- src/Pi/robofriend.py
+++ src/Pi/robofriend.py
@@ -6,13 +6,10 @@ import time
 import sys
 import os
 import rospy
-
+import roslaunch
 
 path = str(os.getcwd()) + "/python"
 sys.path.append(path)
-
-
-
 
 # own modules
 import faceModule as faceModule
@@ -25,7 +22,7 @@ import teensyCommunicator as teensyCommunicator
 #import speechModule as speechModule
 #import cam_node as cam_node
 #import facedetectionModule as facedetectionModule
-import systemModule as systemModule
+from systemModule import *
 
 # import ROS modules
 #from ROS_Node.FacedetectionNode import *
@@ -41,83 +38,36 @@ from ROS_Node.ServoCamNode import *
 from ROS_Node.IOWarriorNode import *
 
 # globals
-runFlag = True
+roscore = None
+rosrobo = None
 
 def stop():
-	global runFlag
+	global roscore
 
-	print("*** shutting down ... ***")
-	systemModule.roscore_terminate()
-	#rfidModule.stop()
-	rfid_node.node_stop()
-	webserverModule.stop()
-	#statusModule.stop()
-	gameCommunicator.stop()
-	# keyboardModule.stop()
-	keyboard_node.node_stop()
-	teensyCommunicator.stop()
-	ioWarriorModule.stop()
-	# speechModule.stop()
-	faceModule.close()
-	#cam_node.node_stop()
-	robobrain_node.node_stop()
-	bat_inf_node.node_stop()
-	runFlag = False
-	print("*** graceful shutdown completed! ***")
+	print("*** shutting down! ... ***\n\n")
+	roscore.terminated()
 
 def handler_stop_signals(signum, frame):
 	stop()
 
 def main():
-	global runFlag
+	global roscore, rosrobo
 
-	print("RoboFriend Main Script Ready...")
-	print("Starting RosMaster!")
-	systemModule.roscore_start()
-	print("Initialising RosPy!")
-	rospy.init_node('Robofriend_node', anonymous = True)
-	print("Done ... starting Robobrain node")
-	robobrain_node.node_start()
-	print("Done ... starting RFID!")
-	#rfidModule.start()
-	rfid_node.node_start()
-	print("Done ... starting Webserver!")
-	webserverModule.start()
-	print("Done ... starting StatusModule!")
-	#statusModule.start()
-	print("Done ... starting Gamecommunicator")
-	gameCommunicator.start()
-	print("Done ... starting KeyboardModule")
-	#keyboardModule.start()
-	keyboard_node.node_start()
-	print("Done ... starting FaceModue")
-	faceModule.drawFace()
-	print("Done ... starting RosFacedetectionNode")
-	#facedetection_node.node_start()
-	print("Done ... starting FacedetectListener")
-	#facedetectionModule.listener()
-	print("Done ... start Speech Node")
-	speech_node.node_start()
-	print("Done ... start Teensy Node")
-	teensy_node.node_start()
-	print("Done ... start Battery/Infrared Node")
-	bat_inf_node.node_start()
-	print("Done ... start Ears/Led Node")
-	ears_led_node.node_start()
-	print("Done ... start Servo Camera Node")
-	servo_cam_node.node_start()
-	print("Done ... start IOWarrior Node")
-	iowarrior_node.node_start()
-	print("init done! register signal handlers...")
+	print("RoboFriend Main Script Ready ...")
+	print("Starting Roscore!")
+	roscore = Roscore()
+	roscore.run()
+	print("Starting Robofriend Startup ...")
+	rosrobo = RosRobo()
+	rosrobo.run()
+	
+	# TODO: do we need signal handlers?! 
+	print("Initialization done! Registering keyboard interrput signal handlers ...")
 
 	# setting up signal handlers for shutdown
 	signal.signal(signal.SIGINT, handler_stop_signals)
 	signal.signal(signal.SIGTERM, handler_stop_signals)
 	print("*** startup completed! ***\n\n")
-
-	#rospy.spin()
-	while runFlag: time.sleep(0.5) # keep program running until stopped
-	print("[INFO] Main script terminated!!!")
 
 if __name__ == '__main__':
 	main()
