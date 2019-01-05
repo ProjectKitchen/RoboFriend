5f8593f20e36d15d98b993cfbeaafbe35560c9a0
momokarl <m.karlitepe@hotmail.com>
2019-01-05 14:07:52 +0100

    Functions to handling max/min messages implemented

--- src/Pi/catkin_ws/src/robofriend/scripts/ServoCamNode/ServoCamDataHandler.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/ServoCamNode/ServoCamDataHandler.py
@@ -3,30 +3,38 @@ import rospy
 
 # import ros message
 from ros_robofriend.msg import IOWarriorData
-from std_msgs.msg import Int8
-
+from ros_robofriend.msg import ServoCamData
 
 class ServoCamDataHandler():
 
     def __init__(self):
         self.__cam_pos = 140
         self.__diff = 0
+        self.__max_min = None
 
         self.__iowarrior_pub = rospy.Publisher('T_IOWARRIOR_DATA', IOWarriorData, queue_size = 10)
         self.__iowarrior_msg = IOWarriorData()
 
     def process_data(self, data):
-        self.__diff = data.data
+        self.__max_min = data.max_min
+        self.__diff = data.diff
         print("[INFO] {} - Received Data: {}"
-                .format(self.__class__.__name__, data.data))
+                .format(self.__class__.__name__, data))
         self.__diff_calc()
 
     def __diff_calc(self):
-        if 10 <= self.__cam_pos + self.__diff <= 150:
-            self.__cam_pos += self.__diff
+        if self.__max_min == "max":
+            self.__cam_pos = 150
+            self.__send_to_iowarrior(self.__cam_pos)
+        elif self.__max_min == "min":
+            self.__cam_pos = 10
             self.__send_to_iowarrior(self.__cam_pos)
         else:
-            print("[INFO] Position for the camera too high/low: {}\n".format(self.__cam_pos + self.__diff))
+            if 10 <= self.__cam_pos + self.__diff <= 150:
+                self.__cam_pos += self.__diff
+                self.__send_to_iowarrior(self.__cam_pos)
+            else:
+                print("[INFO] Position for the camera too high/low: {}\n".format(self.__cam_pos + self.__diff))
 
     def __send_to_iowarrior(self, cam_position):
         self.__iowarrior_msg.rgb = []
