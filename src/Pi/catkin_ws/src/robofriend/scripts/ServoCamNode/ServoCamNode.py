ab3e2e304bcf742868e7651904fc985e8a43b5c2
momokarl <m.karlitepe@hotmail.com>
2019-01-05 14:06:49 +0100

    Subscriber due to the change of message type modified

--- src/Pi/catkin_ws/src/robofriend/scripts/ServoCamNode/ServoCamNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/ServoCamNode/ServoCamNode.py
@@ -1,7 +1,7 @@
 import rospy
 
 # import ros message
-from std_msgs.msg import Int8
+from ros_robofriend.msg import ServoCamData
 
 # import ros modules
 from ServoCamNode.ServoCamDataHandler import *
@@ -10,4 +10,4 @@ def node_start():
     print("[INFO] ROS Servo Camera Node started!\n")
 
     servocam = ServoCamDataHandler()
-    rospy.Subscriber("T_SERVO_CAM_DATA", Int8, servocam.process_data)
+    rospy.Subscriber("T_SERVO_CAM_DATA", ServoCamData, servocam.process_data)
