65307027b943d42d49aa61770024726062c0552e
Martin Zahedi <martinzahedi@gmail.com>
2019-01-06 18:44:38 +0100

    should run on other pcs

--- src/Pi/catkin_ws/src/robofriend/scripts/IOWarriorNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/IOWarriorNode.py
@@ -3,12 +3,10 @@ import os
 import rospy
 
 # import ros message
-from ros_robofriend.msg import IOWarriorData
+from robofriend.msg import IOWarriorData
 
 def send_to_iowarrior(self, red = 0, green = 0, blue = 0, cam_pos = 0):
-    cmd = "sudo ./iowarrior/iow " + str(int(round(red))) 
-                            + ' ' + str(int(round(green))) 
-                            + ' ' + str(int(round(blue)))
+    cmd = "sudo ./iowarrior/iow " + str(int(round(red))) + ' ' + str(int(round(green))) + ' ' + str(int(round(blue)))
     if cam_pos:
         cmd = cmd + ' ' + str(cam_pos)
     rospy.logdebug("{%} - CMD for IOWarrior:  %s", self.__class__.__name__, cmd)
