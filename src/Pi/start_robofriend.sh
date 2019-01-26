00bebf2fb1db180af107344b915f85be51bfc759
mzahedi <martinzahedi@gmail.com>
2019-01-26 21:12:18 +0100

    adding turtlebot3 submodules

--- src/Pi/start_robofriend.sh
+++ src/Pi/start_robofriend.sh
@@ -1,7 +1,7 @@
 # cd mjpg
 mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 320 -y 240 -quality 30 --fps 15" &
 # build catkin workspace
-cd catkin_ws/ && catkin_make clean && catkin_make && catkin_make install && cd ..
+# cd catkin_ws/ && catkin_make clean && catkin_make && catkin_make install && cd ..
 source ~/.bashrc
 python3 start_robofriend.py
 
