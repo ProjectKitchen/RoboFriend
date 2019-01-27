3f4420044537ce5c35656982e8c472dc48cc766e
momokarl <m.karlitepe@hotmail.com>
2019-01-27 11:18:26 +0100

    bug fix

--- src/Pi/catkin_ws/src/robofriend/scripts/LedEarsNode/LedEarsNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/LedEarsNode/LedEarsNode.py
@@ -11,18 +11,18 @@ def node_start():
     print("[INFO] ROS Ears/Led Node started!\n")
 
     # thread to handle incoming messages from brain node
-    ears_led_thread = threading.Thread(
+    led_ears_thread = threading.Thread(
         target = handle_ears_led
     )
 
     # start thread as a daeomon
-    ears_led_thread.daemon = True
+    led_ears_thread.daemon = True
 
     # start ears/led thread
-    ears_led_thread.start()
+    led_ears_thread.start()
 
 def handle_ears_led():
 
     print("[INFO] Thread for ears_led started !")
-    ears_led = EarsLedDataHandler()
-    rospy.Subscriber('T_EARS_LED_DATA', LedEarsData, ears_led.process_data)
+    led_ears = LedEarsDataHandler()
+    rospy.Subscriber('T_LED_EARS_DATA', LedEarsData, led_ears.process_data)
