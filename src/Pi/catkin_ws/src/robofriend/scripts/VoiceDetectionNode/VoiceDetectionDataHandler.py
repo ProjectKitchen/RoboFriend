d706b88c04f23f4acbe5fc6cb98a1e11ac1fc721
momokarl <m.karlitepe@hotmail.com>
2019-02-17 08:59:40 +0100

    create class and add service handler to activate a specific hotword

--- src/Pi/catkin_ws/src/robofriend/scripts/VoiceDetectionNode/VoiceDetectionDataHandler.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/VoiceDetectionNode/VoiceDetectionDataHandler.py
@@ -1,4 +1,17 @@
 import rospy
 
 # import ros messages and services
-from robofriend.
+from robofriend.srv import SrvVoiceHotwordActivation, SrvVoiceHotwordActivationResponse
+
+class VoiceDetectionDataHandler():
+
+    def __init__(self):
+
+        # declare voice hotword detection service
+         serv = rospy.Service('/robofriend/voicehotword', SrvVoiceHotwordActivation, self._voice_hotword_service_handler)
+
+    def _voice_hotword_service_handler(self, request):
+        rospy.logdebug("{%s} - Voice hotword activation request received!\n",
+            self.__class__.__name__)
+
+        
