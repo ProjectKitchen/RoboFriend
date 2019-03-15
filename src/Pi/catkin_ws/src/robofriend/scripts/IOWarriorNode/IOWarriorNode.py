3decb153c879186d6c8c8c1bb66884acebf0b120
momokarl <m.karlitepe@hotmail.com>
2019-03-15 11:22:09 +0100

    module rewritten in order to start node through launch file

--- src/Pi/catkin_ws/src/robofriend/scripts/IOWarriorNode/IOWarriorNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/IOWarriorNode/IOWarriorNode.py
@@ -1,13 +1,52 @@
+#!/usr/bin/env python
 import rospy
+import os
 
 # import ros message
 from robofriend.msg import IOWarriorData
 
 # import ros modules
-from IOWarriorNode.IOWarriorDataHandler import *
+from robofriend.msg import IOWarriorData
 
-def node_start():
-    print("[INFO] ROS IOWarrior Node started!\n")
+class IOWarriorDataHandler():
+
+    def __init__(self):
+        self.__red = 0
+        self.__green = 10
+        self.__blue = 10
+        self.__cam_pos = 140
+
+        self.__send_to_iowarrior(self.__red, self.__green, self.__blue, self.__cam_pos)
+
+    def process_data(self, data):
+        rospy.loginfo("{%s} - Received message: %s, %s\n",
+            self.__class__.__name__, str(data.rgb), str(data.cam_pos))
+        if data.rgb:
+            self.__red, self.__green, self.__blue = data.rgb
+        if data.cam_pos:
+            self.__cam_pos = data.cam_pos
+
+        self.__send_to_iowarrior(self.__red, self.__green, self.__blue, self.__cam_pos)
+
+    def __send_to_iowarrior(self, red = 0, green = 0, blue = 0, cam_pos = 0):
+        cmd = "sudo ./iowarrior/iow " + str(int(round(red))) + ' ' + str(int(round(green))) + ' ' + str(int(round(blue)))
+        if cam_pos:
+            cmd = cmd + ' ' + str(cam_pos)
+        rospy.loginfo("{%s} - CMD command for IOWarrior: %s",
+            self.__class__.__name__, cmd)
+        os.system(cmd)
+
+def IOWarrior():
+    rospy.init_node("robofriend_io_warrior_handler", log_level = rospy.INFO)
+    rospy.loginfo("Starting IOWarrior Data Handler Node")
 
     iowarrior = IOWarriorDataHandler()
     rospy.Subscriber('/robofriend/io_warrior_data', IOWarriorData, iowarrior.process_data)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        IOWarrior()
+    except rospy.ROSInterruptException:
+        pass
--- src/Pi/catkin_ws/src/robofriend/scripts/KeyboardNode/KeyboardNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/KeyboardNode/KeyboardNode.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # external modules
 import pygame
 import sys
@@ -10,89 +11,92 @@ import rospy
 # import ros message
 from robofriend.msg import KeyboardData
 
-# own modules
-from KeyboardNode.KeyboardDataHandler import *
 
-# globals
-runFlag = True
+class KeyboardDataHandler():
+    def __init__(self):
 
-def node_stop():
-    global runFlag
-    print("[INFO] Stopping keboard node!")
-    runFlag = False
+        self._special_button_key = {
+            8 : "backspace", \
+            13 : "enter", \
+            27 : "escape", \
+            32 : "space", \
+            273 : "up", \
+            274 : "down", \
+            275 : "right", \
+            276 : "left"
+        }
 
-def node_start():
-    print("[INFO] ROS Keyboard Node started!\n")
+        self._pub = rospy.Publisher('/robofriend/keyb_data', KeyboardData, queue_size = 10)
+        self._msg = KeyboardData()
 
-    # thread to handle the keyboard inputs
-    keyboard_thread = threading.Thread(
-        target = handle_keyboard
-    )
-
-    #start thread as daemon
-    keyboard_thread.daemon = True
-
-    # start keyboard thread
-    keyboard_thread.start()
-
-def handle_keyboard():
-    global runFlag
-
-    # special buttons which are processed others ignored
-    special_button_key = {
-        8 : "backspace", \
-        13 : "enter", \
-        27 : "escape", \
-        32 : "space", \
-        273 : "up", \
-        274 : "down", \
-        275 : "right", \
-        276 : "left"
-    }
-
-    publish_message = {}
-
-    pub = rospy.Publisher('/robofriend/keyb_data', KeyboardData, queue_size = 10)
-    msg = KeyboardData()
-
-    keyboard = KeyboardDataHandler(pub, msg)
-
-    while runFlag:
+    def _keyboard(self):
+        publish_message = {}
         try:
             event = pygame.event.wait()
             if 'mod' in event.__dict__ and 'unicode' in event.__dict__:
                 if event.type == pygame.QUIT:
-                    print("[INFO] Quit pressed")
-                    publish_message = message_dict_merge(quit = True)
+                    rospy.logdebug("{%s} - Quit pressed\n")
+                    publish_message = self._message_dict_merge(quit = True)
                 if event.__dict__["mod"] == 12288:
-                    if event.key in special_button_key:
-                        publish_message = message_dict_merge(up_down = "up", pressed_key = special_button_key[event.key])
+                    if event.key in self._special_button_key:
+                        publish_message = self._message_dict_merge(up_down = "up", pressed_key = self._special_button_key[event.key])
                     else:
                         if event.__dict__['unicode']:
-                            publish_message = message_dict_merge(up_down = "up", pressed_key = event.__dict__['unicode'])
+                            publish_message = self._message_dict_merge(up_down = "up", pressed_key = event.__dict__['unicode'])
                         else:
                             pass
                 elif event.type == pygame.KEYDOWN:
-                    if event.key in special_button_key:
-                        publish_message = message_dict_merge(pressed_key = special_button_key[event.key])
+                    if event.key in self._special_button_key:
+                        publish_message = self._message_dict_merge(pressed_key = self._special_button_key[event.key])
                     else:
                         if event.__dict__['unicode']:
-                            publish_message = message_dict_merge(pressed_key = event.__dict__['unicode'])
+                            publish_message = self._message_dict_merge(pressed_key = event.__dict__['unicode'])
                         else:
                             pass
                 else:
                     pass
                 if bool(publish_message) == True:
-                    keyboard.message_publish(publish_message)
-                    publish_message.clear()
+                    self._message_publish(publish_message)
                 else:
                     pass
         except Exception as e:
-            print("keyboard exception: {}".format(e))
-            print(traceback.format_exc())
+            rospy.logdebug("{%s} - Keyboard exception arised: %s\n",
+                self.__class__.__name__, e)
 
-def message_dict_merge(quit = False, up_down = "down", pressed_key = ""):
-    message = {'quit' : quit, \
-               'up_down' : up_down, \
-               'pressed_key' : pressed_key}
-    return message
+    def _message_dict_merge(self, quit = False, up_down = "down", pressed_key = ""):
+        message = {'quit' : quit, \
+                   'up_down' : up_down, \
+                   'pressed_key' : pressed_key}
+        return message
+
+    def _message_publish(self, message):
+        self._msg.quit = message ['quit']
+        self._msg.up_down = message ['up_down']
+        self._msg.pressed_key = message ['pressed_key']
+        self._pub.publish(self._msg)
+        rospy.logwarn("{%s} - Published Keyboard Message: %s\n",
+            self._msg)
+
+def shutdown():
+    rospy.loginfo("{%s} - stopping keyboard data handler.", rospy.get_caller_id())
+    rospy.signal_shutdown("Stopping Keyboard node!")
+
+
+def Keyboard():
+    rospy.init_node("robofriend_keyboard_node", log_level = rospy.INFO)
+    rospy.loginfo("Starting Keyboard Node!")
+
+    kb = KeyboardDataHandler()
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        kb._keyboard()
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        Keyboard()
+    except rospy.ROSInterruptException:
+        pass
