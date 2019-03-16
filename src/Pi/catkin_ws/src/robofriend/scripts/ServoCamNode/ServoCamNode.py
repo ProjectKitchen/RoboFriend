e9d7d0166cd888864eb329212c8e0f67ad50305d
momokarl <m.karlitepe@hotmail.com>
2019-03-16 15:37:03 +0100

    logic to hanlde incoming messages from the service implemented

--- src/Pi/catkin_ws/src/robofriend/scripts/ServoCamNode/ServoCamNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/ServoCamNode/ServoCamNode.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import rospy
 
-# import ros message
+# import ros message and services
 from robofriend.msg import IOWarriorData
 from robofriend.msg import ServoCamData
+from robofriend.srv import SrvServoCameraData, SrvServoCameraDataResponse
 
 def shutdown():
     rospy.loginfo("{%s} - stopping servo/cam data handler", rospy.get_caller_id())
@@ -19,7 +20,16 @@ class ServoCamDataHandler():
         self.__iowarrior_pub = rospy.Publisher('/robofriend/io_warrior_data', IOWarriorData, queue_size = 10)
         self.__iowarrior_msg = IOWarriorData()
 
-    def process_data(self, data):
+        # Webserver services
+        rospy.Service('/robofriend/camera_position', SrvServoCameraData, self._service_handler)
+
+    def _service_handler(self, request):
+        rospy.logdebug("{%s} - Camera Change request received!",
+            rospy.get_caller_id())
+        self.__diff = request.cam_change
+        self.__diff_calc()
+
+    def _process_data(self, data):
         self.__max_min = data.max_min
         self.__diff = data.diff
         rospy.logdebug("{%s} - Received data: %s",
@@ -40,7 +50,7 @@ class ServoCamDataHandler():
                 self.__send_to_iowarrior(self.__cam_pos)
             else:
                 rospy.logdebug("{%s} - Position for camera to high/low: %s",
-                               str(self.__cam_pos + self.__diff))
+                    rospy.get_caller_id(), str(self.__cam_pos + self.__diff))
 
     def __send_to_iowarrior(self, cam_position):
         self.__iowarrior_msg.rgb = []
@@ -55,7 +65,7 @@ def ServoCam():
 
     servocam = ServoCamDataHandler()
     rospy.Subscriber("/robofriend/servo_cam_data",
-                ServoCamData, servocam.process_data)
+                ServoCamData, servocam._process_data)
 
     rospy.spin()
 
