c80ff1014f0b1db90a6441fe5e7ba1429c85dc01
momokarl <m.karlitepe@hotmail.com>
2019-03-18 12:58:10 +0100

    rewritting faceModule to to ROS Node / functions implemented and first testings accomplished

---
+++ src/Pi/catkin_ws/src/robofriend/scripts/FaceNode.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+import rospy
+import time
+import pygame
+import pygame.gfxdraw
+import constants
+
+# import ros messages and services
+from robofriend.srv import SrvFaceDrawData, SrvFaceDrawDataResponse
+
+class FaceDataHandler():
+
+    def __init__(self):
+
+        pygame.init()
+        self._screen = pygame.display.set_mode((654, 380)) # pygame.display.set_mode((654, 380), pygame.FULLSCREEN)
+        self._screen.fill((0, 0, 0))
+
+        self._eyes_x = 0
+        self._eyes_y = 0
+        self._smile_percent = 60
+        self._eye_step = 10
+        self._screenshot_filename = 'screenshot.jpg'
+
+        self._face_meth = {'get_screen_fn':   self._get_screenshot_filename, \
+                           'set_smile':       self._set_smile, \
+                           'increase_smile':  self._increase_smile, \
+                           'decrease_smile':  self._decrease_smile, \
+                           'set_eyes':        self._set_eyes, \
+                           'eyes_up':         self._eyes_up, \
+                           'eyes_down':       self._eyes_down, \
+                           'eyes_left':       self._eyes_left, \
+                           'eyes_right':      self._eyes_right
+        }
+
+        self.FIRST_ELEM = 0
+        self.SECOND_ELEM = 1
+
+        self._meth = ""
+        self._param = []
+
+        # service to communicate to sound node
+        #rospy.Service('/robofriend/face_sound_stat', SrvFacSoundData,
+        #    self._sound_service_handler)
+
+    def _sound_service_handler(self, request):
+        rospy.logdebug("{%s} - Request regarding status from sound node received!",
+            rospy.get_caller_id())
+
+        resp = False
+        is_sad = False
+
+        if request.is_sad:
+            resp = True
+            is_sad = self._is_sad()
+            rospy.logdebug("{%s} - Service response message: %s",
+                    rospy.get_caller_id(), is_sad)
+        else:
+            resp = False
+            is_sad = False
+
+        rospy.logdebug("{%s} - Service response message: %s, %s",
+            rospy.get_caller_id(), resp, is_sad)
+        return SrvFaceDrawDataResponse(resp, is_sad)
+
+
+    def _service_handler(self, request):
+        rospy.logdebug("{%s} - Face request received: %s, %s",
+            rospy.get_caller_id(), str(request.action), str(request.param))
+
+        fn = ""
+        resp = False
+
+        self._meth = request.action
+        self._param = request.param
+
+        if self._meth in constants.SET_SMILE or \
+           self._meth in constants.SET_EYES:
+                resp, fn = self._face_meth.\
+                        get(self._meth, self._errorhandler)(self._param)
+                rospy.logdebug("{%s} - Service response message: %s, %s",
+                        rospy.get_caller_id(), res, fn)
+        elif self._meth in self._face_meth:
+                resp, fn = self._face_meth.\
+                        get(self._meth, self._errorhandler)()
+                rospy.logdebug("{%s} - Service response message: %s, %s",
+                    rospy.get_caller_id(), resp, fn)
+        self._draw_face()
+        return SrvFaceDrawDataResponse(resp, fn)
+
+    def _errorhandler(self):
+        rospy.logwarn("{%s} - Wrong methode received!",
+            rospy.get_caller_id())
+        return False, None
+
+    def _get_screenshot_filename(self):
+        return True, self._screenshot_filename
+
+    def _set_smile(self, param):
+        retVal = False
+        smile_perc = 0
+
+        if len(param) is not 1:
+            retVal = False
+        else:
+            self._smile_percent = \
+            self._restrict_range(param[self.FIRST_ELEM], -100, 100)
+            retVal = True
+        return retVal, None
+
+    def _increase_smile(self):
+        self._smile_percent = \
+            self._restrict_range(self._smile_percent + 10, -100, 100)
+        return True, None
+
+    def _decrease_smile(self):
+        self._smile_percent = \
+                self._restrict_range(self._smile_percent - 10, -100, 100)
+        return True, None
+
+    def _set_eyes(param):
+        x_percent = param[self.FIRST_ELEM]
+        y_percent = param[self.SECOND_ELEM]
+
+        if x_percent < -100:
+            x_percent = -100
+        elif x_percent > 100:
+            x_percent = 100
+
+        if y_percent < -100:
+            y_percent = 100
+        elif y_percent > 100:
+            y_percent = 100
+        eyex = int(x_percent * (40.0 / 100))
+        eyey = int(y_percent * (40.0 / 100))
+        return True, None
+
+    def _eyes_up(self):
+        if self._eyes_y > -40:
+            self._eyes_y -= self._eye_step
+        return True, None
+
+    def _eyes_down(self):
+        if self._eyes_y < 40:
+            self._eyes_y += self._eye_step
+        return True, None
+
+    def _eyes_left(self):
+        if self._eyes_x > -40:
+            self._eyes_x -= self._eye_step
+        return True, None
+
+    def _eyes_right(self):
+        if self._eyes_x < 40:
+            self._eyes_x += self._eye_step
+        return True, None
+
+    def _is_sad(self):
+        return self._smile_percent < 0
+
+    def _restrict_range(self, value, minValue, maxValue):
+        return value if minValue <= value <= maxValue else (minValue if value < minValue else maxValue)
+
+    def _draw_face(self):
+        self._screen.fill((0, 0, 0))
+        pygame.draw.circle(self._screen, (100, 250, 250), (163,100), 60) #lefteye
+        pygame.draw.circle(self._screen, (100, 250, 250), (491,100), 60) #righteye
+        pygame.draw.circle(self._screen, (10, 10, 10), (163 + self._eyes_x,100 + self._eyes_y), 20) #leftpupil
+        pygame.draw.circle(self._screen, (10, 10, 10), (491 + self._eyes_x,100 + self._eyes_y), 20) #rightpupil
+        self._draw_mouth()
+        pygame.display.flip()
+        pygame.image.save(self._screen, self._screenshot_filename)
+        # MOMOKARL: maybe a better way to solve this?
+        #statusModule.setScreenshotTimestamp() ??????
+
+    def _draw_mouth(self):
+        if self._smile_percent < 0:
+            radius = self._map_range(abs(self._smile_percent), 0, 100, 0.3, 0.9)
+            pygame.draw.arc(self._screen, (100, 200, 200), (57, 300, 540, 400), 1.57 - radius, 1.57 + radius, 20)  # smile
+        else:
+            radius = self._map_range(abs(self._smile_percent), 0, 100, 0.3, 1.3)
+            pygame.draw.arc(self._screen, (100, 200, 200), (57, -30, 540, 400), 4.7 - radius, 4.7 + radius, 20)
+
+    def _map_range(self, value, left_min, left_max, right_min, right_max):
+        # Figure out how 'wide' each range is
+        left_span = left_max - left_min
+        right_span = right_max - right_min
+
+        # Convert the left range into a 0-1 range (float)
+        value_scaled = float(value - left_min) / float(left_span)
+
+        # Convert the 0-1 range into a value in the right range.
+        return right_min + (value_scaled * right_span)
+
+def shutdown():
+    rospy.loginfo("{%s} - stopping face node!",
+        rospy.get_caller_id())
+    pygame.quit()
+    rospy.signal_shutdown("Stopping face node")
+
+def Face():
+    rospy.init_node("robofriend_face", log_level = rospy.DEBUG)
+    rospy.loginfo("{%s} - starting face node!",
+        rospy.get_caller_id())
+
+
+    face = FaceDataHandler()
+
+    # create Service
+    rospy.Service('/robofriend/face', SrvFaceDrawData, face._service_handler)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        Face()
+    except rospy.ROSInterruptException:
+        pass
