6e4f86af45f99cea225e115a86f998e1be68bc31
momokarl <m.karlitepe@hotmail.com>
2019-03-26 03:32:40 +0100

    logic to enable playing sound during the game is added

--- src/Pi/catkin_ws/src/robofriend/scripts/GameCommunicator.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/GameCommunicator.py
@@ -20,49 +20,50 @@ BAT_VOLT = 0
 FACEMOD_SRV_REQ_ACTION = None
 FACEMOD_SRV_REQ_PARAM = None
 TEENSY_SRV_REQ = None
+SOUND_SRV_REQ = None
 
 def dataListener():
-    """ 
+    """
     this function is used as a pseudo thread to always listen if there is a client (gamegui) sending commands
     """
     global IP
- 
+
     startFlag = ":RUN:"
     endFlag = ":EOL:"
     receivedData = ""
-    
-    try: 
+
+    try:
         tempData, addr = UDP_SOCKET.recvfrom(50)
     except Exception as inst:
-        # we want to have debug level here since we have a non-blocking socket which 
-        # will make the socket always 'temporarily  unavailable' in the loop 
+        # we want to have debug level here since we have a non-blocking socket which
+        # will make the socket always 'temporarily  unavailable' in the loop
         rospy.logdebug('{%s} - this is a controlled catch.', rospy.get_caller_id())
         rospy.logdebug('{%s} - could not receive data from udp socket.', rospy.get_caller_id())
         rospy.logdebug('{%s} - exception type: %s', rospy.get_caller_id(), type(inst))
         rospy.logdebug('{%s} - exception argument: %s', rospy.get_caller_id(), inst.args[1])
         return
-        
+
     receivedData += tempData.decode("utf-8")
     startFlagIndex = receivedData.find(startFlag)
     if startFlagIndex != -1:
         receivedData = receivedData[startFlagIndex + len(startFlag):]
- 
+
     endFlagIndex = receivedData.find(endFlag)
     if endFlagIndex != -1:
         receivedData = receivedData[:endFlagIndex]
     else:
-        rospy.logwarn("{%s} - expected string format: \":RUN:xxx:EOL:\", your string: %s", 
+        rospy.logwarn("{%s} - expected string format: \":RUN:xxx:EOL:\", your string: %s",
                       rospy.get_caller_id(), receivedData)
         return
- 
+
     # make sure we answer the caller
     IP = addr[0]
-    rospy.loginfo("{%s} - received data: \"%s\", from app: %s", 
+    rospy.loginfo("{%s} - received data: \"%s\", from app: %s",
                   rospy.get_caller_id(), receivedData, str(IP))
     chooseAction(receivedData)
 
 def chooseAction(data):
-    global currentStatus
+    global currentStatus, FACE_SRV_REQ
     dataArray = data.split(';') # Nachricht wird in ein Array gespeichert
     action = dataArray[0] # erstes Argument
     dataArray = dataArray[1:] # restliche Argumente
@@ -74,13 +75,14 @@ def chooseAction(data):
     # MOMOKARL: how do we solve this?
     # best way would be by implementing a service
     # see the example above with move
-#     elif action == "say":
-#         speechModule.speak(dataArray[0])
-#     elif action == "sound":
-#         info = dataArray[0]
-#         dataArray = dataArray[1:]
-#         if info == "play":
-#             soundModule.playsound(dataArray)
+    elif action == "say":
+        speechModule.speak(dataArray[0])
+    elif action == "sound":
+        info = dataArray[0]
+        dataArray = dataArray[1:]
+        if info == "play":
+            SOUND_SRV_REQ = dataArray
+            # soundModule.playsound(dataArray)
     elif action == "face":
         # echo -n ":RUN:face;smile;increase:EOL:" >/dev/udp/localhost/9000
         # echo -n ":RUN:face;eyes;up:EOL:" >/dev/udp/localhost/9000
@@ -102,7 +104,7 @@ def move(dataArray):
         # TODO: fuer spaeter hier erweiterung moeglich,
         # auf laenge der steps eingehen, jetzt nur standardwert verwendet
         # dataArray = dataArray[1:]
-        # step = dataArray[0] 
+        # step = dataArray[0]
         options = {'forward':       constants.MOVE_STEP_FWD,
                    'right':         constants.MOVE_STEP_RYT,
                    'backward':      constants.MOVE_STEP_BCK,
@@ -111,7 +113,7 @@ def move(dataArray):
         if dir in options:
             TEENSY_SRV_REQ = options[dir]
     # keine step informationen vorhanden, daher loop, joystick verwendet
-    else: 
+    else:
         options = {'forward':       constants.MOVE_LOOP_FWD,
                    'right':         constants.MOVE_LOOP_RYT,
                    'backward':      constants.MOVE_LOOP_BCK,
@@ -127,10 +129,10 @@ def move(dataArray):
 
 def manipulateFace(dataArray):
     global FACEMOD_SRV_REQ_ACTION, FACEMOD_SRV_REQ_PARAM, TEENSY_SRV_REQ
-    
+
     objects = ["smile", "eyes"]
     param = []
-    
+
     faceObj, command = dataArray[:2]
     if faceObj in objects:
         FACEMOD_SRV_REQ_ACTION = command
@@ -141,15 +143,15 @@ def manipulateFace(dataArray):
         elif command in "wrong":
             param = [-60]
             TEENSY_SRV_REQ = 'H'
-        
+
         FACEMOD_SRV_REQ_ACTION = constants.SET_SMILE
         FACEMOD_SRV_REQ_PARAM = param
     else:
         rospy.logwarn("{%s} - wrong command for the face module", rospy.get_caller_id())
 
 def sendToGUI(data):
-    """ 
-    this function is used to send information to the gamegui and can be called 
+    """
+    this function is used to send information to the gamegui and can be called
     by the the methode provideRFIDNumber (for RFID data) and chooseAction (for battery information)
     """
     global IP, UDP_PORT, UDP_SOCKET
@@ -169,9 +171,10 @@ def sendToGUI(data):
         rospy.logwarn('{%s} - failed to send data via udp socket.', rospy.get_caller_id())
         rospy.logwarn('{%s} - exception type: %s', rospy.get_caller_id(), type(inst))
         rospy.logwarn('{%s} - exception argument: %s', rospy.get_caller_id(), inst.args[1])
-    
+
 def faceModuleThreadHandler():
     global FACEMOD_SRV_REQ_ACTION, FACEMOD_SRV_REQ_PARAM
+
     while True:
         # send data to teensy per service request param
         if FACEMOD_SRV_REQ_ACTION is not None:
@@ -184,10 +187,10 @@ def faceModuleThreadHandler():
                 FACEMOD_SRV_REQ_ACTION = None
             except rospy.ServiceException:
                 rospy.logwarn("{%s} - service call failed. check the face module.", rospy.get_caller_id())
-                
+
         # maintain the thread at the needed frequency
         time.sleep(1)
-    
+
 def RFIDModuleThreadHandler():
     while True:
         # get data from rfid reader
@@ -199,12 +202,12 @@ def RFIDModuleThreadHandler():
             srv_resp = request(True)
         except rospy.ServiceException:
             rospy.logwarn("{%s} - service call failed. check the rfid serial data.", rospy.get_caller_id())
-        
+
         # process the response
         provideRFIDNumber(srv_resp)
         # maintain the thread at the needed frequency
         time.sleep(1)
-    
+
 def teensyModuleThreadHandler():
     global TEENSY_SRV_REQ
     while True:
@@ -219,18 +222,34 @@ def teensyModuleThreadHandler():
                 TEENSY_SRV_REQ = None
             except rospy.ServiceException:
                 rospy.logwarn("{%s} - service call failed. check the teensy serial data.", rospy.get_caller_id())
-                
+
+        # maintain the thread at the needed frequency
+        time.sleep(1)
+
+def soundModuleThreadHandler():
+    global SOUND_SRV_REQ
+    while True:
+        rospy.wait_for_service('/robofriend/sound')
+        if SOUND_SRV_REQ is not None:
+            try:
+                request = rospy.ServiceProxy('/robofriend/sound', SrvSoundData)
+                # the service response is irrelevant since we dont need to read the response from the teensy dk
+                request(SOUND_SRV_REQ)
+                # make sure to clear the service request parameter
+                SOUND_SRV_REQ = None
+            except rospy.ROSException:
+                rospy.logwarn("{%s} - service call failed. check the sound module.", rospy.get_caller_id())
         # maintain the thread at the needed frequency
         time.sleep(1)
 
 def provideRFIDNumber(args):
     if args is None:
-        return 
+        return
     if args.data is '':
         return
-    
+
     sendToGUI("rfid;" + str(args.data))
-                
+
 def readBatteryVoltage(args):
     if args.voltage <= 0:
         return
@@ -247,14 +266,14 @@ def shutdown():
 def GameCommunicator():
     global t1, t2, t3
     global UDP_SOCKET
-    
+
     rospy.init_node("robofriend_game_communicator", log_level = rospy.INFO)
     rospy.loginfo("{%s} - starting game communicator handler node!", rospy.get_caller_id())
     rospy.on_shutdown(shutdown)
-    
+
     # subscribe to this topic to be able to send the battery voltage via socket
     rospy.Subscriber("/robofriend/pcb_sensor_data", PCBSensorData, readBatteryVoltage)
-    
+
     UDP_IP = ''
     # handle commandline arguments to get ip address
     if len(sys.argv) == 2:
@@ -270,9 +289,9 @@ def GameCommunicator():
             sys.exit()
     else:
         UDP_IP = ''
-    
+
     # connect the udp port
-    try: 
+    try:
         UDP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         UDP_SOCKET.bind((UDP_IP, UDP_PORT))
         UDP_SOCKET.setblocking(0)
@@ -282,10 +301,10 @@ def GameCommunicator():
         rospy.logerr('{%s} - could not open udp socket.', rospy.get_caller_id())
         rospy.logerr('{%s} - exception type: %s', rospy.get_caller_id(), type(inst))
         rospy.logerr('{%s} - exception argument: %s', rospy.get_caller_id(), inst.args[1])
-        
+
     # create threads
     try:
-        t1 = threading.Thread(target=faceModuleThreadHandler, name='face-module-thread') 
+        t1 = threading.Thread(target=faceModuleThreadHandler, name='face-module-thread')
         t1.daemon = True
         t1.start()
         t2 = threading.Thread(target=RFIDModuleThreadHandler, name='rfid-reader-thread')
@@ -294,19 +313,22 @@ def GameCommunicator():
         t3 = threading.Thread(target=teensyModuleThreadHandler, name='teensy-reader-thread')
         t3.daemon = True
         t3.start()
+        t4 = threading.Thread(target=soundModuleThreadHandler, name='sound-module-thread')
+        t4.daemon = True
+        t4.start()
     except Exception as inst:
         rospy.logerr('{%s} - this is a controlled catch.', rospy.get_caller_id())
         rospy.logerr('{%s} - unable to start threads.', rospy.get_caller_id())
         rospy.logerr('{%s} - exception type: %s', rospy.get_caller_id(), type(inst))
         rospy.logerr('{%s} - exception argument: %s', rospy.get_caller_id(), inst.args[0])
 
-    rate = rospy.Rate(1) # 1hz    
-    
+    rate = rospy.Rate(1) # 1hz
+
     while not rospy.is_shutdown():
         # listen to udp port
         dataListener()
         rate.sleep() # make sure the publish rate maintains at the needed frequency
-        
+
 if __name__ == '__main__':
     try:
         GameCommunicator()
