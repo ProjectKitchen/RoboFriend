acde9d9be36d99cec60cee0d191a379e609eee9d
momokarl <m.karlitepe@hotmail.com>
2019-03-28 18:45:09 +0100

    solving IOWarrior Issue

--- src/Pi/catkin_ws/src/robofriend/scripts/IOWarriorNode/IOWarriorNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/IOWarriorNode/IOWarriorNode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import os
+import time
 
 # import ros message
 from robofriend.msg import IOWarriorData
@@ -15,18 +16,27 @@ class IOWarriorDataHandler():
         self.__green = 10
         self.__blue = 10
         self.__cam_pos = 140
+        self._old_time = 0
+        self._valid_time = 0.5
 
         self.__send_to_iowarrior(self.__red, self.__green, self.__blue, self.__cam_pos)
 
     def process_data(self, data):
         rospy.logdebug("{%s} - Received message: %s, %s\n",
             self.__class__.__name__, str(data.rgb), str(data.cam_pos))
+
+        start_time = 0
         if data.rgb:
             self.__red, self.__green, self.__blue = data.rgb
         if data.cam_pos:
             self.__cam_pos = data.cam_pos
 
-        self.__send_to_iowarrior(self.__red, self.__green, self.__blue, self.__cam_pos)
+        if self.__time_request() - self._old_time > self._valid_time:
+            self.__send_to_iowarrior(self.__red, self.__green, self.__blue, self.__cam_pos)
+            self._old_time = self.__time_request()
+        else:
+            rospy.logwarn("{%s} - Command to IOWarrior less then defined Value!",
+                rospy.get_caller_id())
 
     def __send_to_iowarrior(self, red = 0, green = 0, blue = 0, cam_pos = 0):
         cmd = "sudo ./iowarrior/iow " + str(int(round(red))) + ' ' + str(int(round(green))) + ' ' + str(int(round(blue)))
@@ -35,6 +45,9 @@ class IOWarriorDataHandler():
         rospy.logdebug("{%s} - CMD command for IOWarrior: %s",
             self.__class__.__name__, cmd)
         os.system(cmd)
+
+    def __time_request(self):
+        return time.time()
 
 def IOWarrior():
     rospy.init_node("robofriend_io_warrior", log_level = rospy.INFO)
