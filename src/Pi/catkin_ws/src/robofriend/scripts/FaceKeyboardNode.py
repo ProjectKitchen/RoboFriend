248f0a6c13268db726db137849c1d1d0f5a8d85b
momokarl <m.karlitepe@hotmail.com>
2019-03-30 13:09:35 +0100

    Keyboard node into Face Node merged

--- src/Pi/catkin_ws/src/robofriend/scripts/FaceKeyboardNode.py
+++ src/Pi/catkin_ws/src/robofriend/scripts/FaceKeyboardNode.py
@@ -7,6 +7,7 @@ import constants
 import os
 
 # import ros messages and services
+from robofriend.msg import KeyboardData
 from robofriend.srv import SrvFaceDrawData, SrvFaceDrawDataResponse
 from robofriend.srv import SrvFaceStatusData, SrvFaceStatusDataResponse
 from robofriend.srv import SrvFaceScreenshotTimestamp
@@ -208,15 +209,82 @@ class FaceDataHandler():
             rospy.logwarn("{%s} - Erroneous response!",
                     rospy.get_caller_id())
 
+class KeyboardDataHandler():
+    def __init__(self):
+
+        self._special_button_key = {
+            8 : "backspace", \
+            13 : "enter", \
+            27 : "escape", \
+            32 : "space", \
+            273 : "up", \
+            274 : "down", \
+            275 : "right", \
+            276 : "left"
+        }
+
+        self._pub = rospy.Publisher('/robofriend/keyb_data', KeyboardData, queue_size = 10)
+        self._msg = KeyboardData()
+
+    def _keyboard(self):
+        publish_message = {}
+        try:
+            event = pygame.event.wait()
+            if 'mod' in event.__dict__ and 'unicode' in event.__dict__:
+                if event.type == pygame.QUIT:
+                    rospy.logdebug("{%s} - Quit pressed\n")
+                    publish_message = self._message_dict_merge(quit = True)
+                if event.__dict__["mod"] == 12288:
+                    if event.key in self._special_button_key:
+                        publish_message = self._message_dict_merge(up_down = "up", pressed_key = self._special_button_key[event.key])
+                    else:
+                        if event.__dict__['unicode']:
+                            publish_message = self._message_dict_merge(up_down = "up", pressed_key = event.__dict__['unicode'])
+                        else:
+                            pass
+                elif event.type == pygame.KEYDOWN:
+                    if event.key in self._special_button_key:
+                        publish_message = self._message_dict_merge(pressed_key = self._special_button_key[event.key])
+                    else:
+                        if event.__dict__['unicode']:
+                            publish_message = self._message_dict_merge(pressed_key = event.__dict__['unicode'])
+                        else:
+                            pass
+                else:
+                    pass
+                if bool(publish_message) == True:
+                    self._message_publish(publish_message)
+                else:
+                    pass
+        except Exception as inst:
+            rospy.logdebug("{%s} - Keyboard exception arised: %s\n",
+                self.__class__.__name__, inst)
+            rospy.logwarn('{%s} - this is a controlled catch.', rospy.get_caller_id())
+            rospy.logwarn('{%s} - Keyboard error arised.', rospy.get_caller_id())
+            rospy.logwarn('{%s} - exception type: %s', rospy.get_caller_id(), type(inst))
+            rospy.logwarn('{%s} - exception argument: %s', rospy.get_caller_id(), inst.args[0])
+
+    def _message_dict_merge(self, quit = False, up_down = "down", pressed_key = ""):
+        message = {'quit' : quit, \
+                   'up_down' : up_down, \
+                   'pressed_key' : pressed_key}
+        return message
+
+    def _message_publish(self, message):
+        self._msg.quit = message ['quit']
+        self._msg.up_down = message ['up_down']
+        self._msg.pressed_key = message ['pressed_key']
+        self._pub.publish(self._msg)
+
 def shutdown():
-    rospy.loginfo("{%s} - stopping face node!",
+    rospy.loginfo("{%s} - stopping face/keyboard node!",
         rospy.get_caller_id())
     pygame.quit()
-    rospy.signal_shutdown("Stopping face node")
+    rospy.signal_shutdown("Stopping face/keyboard node")
 
-def Face():
-    rospy.init_node("robofriend_face", log_level = rospy.INFO)
-    rospy.loginfo("{%s} - starting face node!",
+def Face_Keyboard():
+    rospy.init_node("robofriend_face_keyboard", log_level = rospy.INFO)
+    rospy.loginfo("{%s} - starting face/keyboard node!",
         rospy.get_caller_id())
 
     # create service to send to make time request
@@ -232,10 +300,16 @@ def Face():
     rospy.Service('/robofriend/face_sound_stat', SrvFaceStatusData,
         face._sound_service_handler)
 
-    rospy.spin()
+    kb = KeyboardDataHandler()
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        kb._keyboard()
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
-        Face()
+        Face_Keyboard()
     except rospy.ROSInterruptException:
         pass
