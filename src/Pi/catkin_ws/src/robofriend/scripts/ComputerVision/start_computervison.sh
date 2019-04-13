a1177dcf039610869f2ba9b6c0848edc97c9e026
momokarl <m.karlitepe@hotmail.com>
2019-04-13 17:16:40 +0200

    Minior changes

--- src/Pi/catkin_ws/src/robofriend/scripts/ComputerVision/start_computervison.sh
+++ src/Pi/catkin_ws/src/robofriend/scripts/ComputerVision/start_computervison.sh
@@ -6,9 +6,18 @@ kill_nodes() {
     trap - SIGINT SIGTERM
 }
 
+
+if [ -z "$1" ]
+then
+    echo "[EROR] No IP address entered!"
+    exit 0
+else
+    ip_address=$1
+fi
+
 trap kill_nodes SIGINT SIGTERM
 
-ip_address=$1
+
 ROS_MASTER_URI="ROS_MASTER_URI=http://$ip_address:11311"
 
 export $ROS_MASTER_URI
